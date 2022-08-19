e212fcc61e7ddb3bc91cf7cf032ea214540872cb
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-08-19 14:49:07 +0200

    Lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ extensions = [
     "sphinx_autodoc_typehints",
     "sphinx_automodapi.automodapi",
     "sphinx_automodapi.smart_resolver",
-    'm2r2',
+    "m2r2",
 ]
 
 
@@ -80,8 +80,8 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-#source_suffix = ".rst"
+source_suffix = [".rst", ".md"]
+# source_suffix = ".rst"
 
 # The master toctree document.
 master_doc = "index"
