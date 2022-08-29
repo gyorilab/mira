de3a93e3aa9d8b3e06843ed6c8e5ffac694cef62
kkaris <karis.klas@gmail.com>
2022-08-29 09:26:10 -0700

    Add concepts test

--- tests/test_template.py
+++ tests/test_template.py
@@ -16,3 +16,15 @@ def test_templates_equal():
     assert c1.is_equal_to(c2, with_context=False)
     assert not c1.is_equal_to(c2_w_ctx, with_context=True)
     assert c1.is_equal_to(c2_w_ctx, with_context=False)
+
+
+def test_concepts_equal():
+    c1 = Concept(name='infected population', identifiers={'ido': '0000511'})
+    c1_w_ctx = c1.with_context(location="Berlin")
+    c2 = Concept(name='infected population', identifiers={'ido': '0000511'})
+    c2_w_ctx = c2.with_context(location="Stockholm")
+
+    assert c1.is_equal_to(c2)
+    assert not c1_w_ctx.is_equal_to(c2_w_ctx, with_context=True)
+    assert c1_w_ctx.is_equal_to(c2_w_ctx, with_context=False)
+
