01901111d6d53025e066bdd40dd0abcf46e1c294
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-09-07 18:29:14 +0200

    Update tests

--- tests/test_dkg.py
+++ tests/test_dkg.py
@@ -1,12 +1,16 @@
 """Tests for the domain knowledge graph app."""
 
+import inspect
+import os
 import unittest
+
+import fastapi.params
+import pystow
 from fastapi.testclient import TestClient
 from gilda.grounder import Grounder
-from mira.dkg.utils import MiraState
 
-import pystow
-import os
+from mira.dkg.api import get_relations
+from mira.dkg.utils import MiraState
 
 MIRA_NEO4J_URL = pystow.get_config("mira", "neo4j_url") or os.getenv("MIRA_NEO4J_URL")
 
@@ -30,16 +34,31 @@ class TestDKG(unittest.TestCase):
         """Test grounding with a get request."""
         response = self.client.get("/api/ground/vaccine")
         self.assertEqual(200, response.status_code, msg=response.content)
-        self.assertTrue(any(
-            r["prefix"] == "vo" and r["identifier"] == "0000001"
-            for r in response.json()["results"]
-        ))
+        self.assertTrue(
+            any(
+                r["prefix"] == "vo" and r["identifier"] == "0000001"
+                for r in response.json()["results"]
+            )
+        )
 
     def test_grounding_post(self):
         """Test grounding with a post request."""
         response = self.client.post("/api/ground", json={"text": "vaccine"})
         self.assertEqual(200, response.status_code, msg=response.content)
-        self.assertTrue(any(
-            r["prefix"] == "vo" and r["identifier"] == "0000001"
-            for r in response.json()["results"]
-        ))
+        self.assertTrue(
+            any(
+                r["prefix"] == "vo" and r["identifier"] == "0000001"
+                for r in response.json()["results"]
+            )
+        )
+
+    def test_get_relations(self):
+        """Test getting relations."""
+        spec = inspect.signature(get_relations)
+        relation_query_default = spec.parameters["relation_query"].default
+        self.assertIsInstance(relation_query_default, fastapi.params.Body)
+
+        for key, data in relation_query_default.examples.items():
+            with self.subTest(key=key):
+                response = self.client.post("/api/relations", json=data["value"])
+                self.assertEqual(200, response.status_code, msg=response.content)
