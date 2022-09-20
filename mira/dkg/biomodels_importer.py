64f2975626b55a0c3c4925f7e6fc17382803b39f
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-09-20 19:59:50 +0200

    Add parsing of SBML documents

--- mira/dkg/biomodels_importer.py
+++ mira/dkg/biomodels_importer.py
@@ -4,18 +4,25 @@ models at https://www.ebi.ac.uk/biomodels/covid-19
 that might be relevant for ingestion into the DKG.
 """
 
-from tabulate import tabulate
 import pystow
+import zipfile
 import requests
 import pandas as pd
+from libsbml import SBMLReader
 
 MODULE = pystow.module("mira")
-BIOMODELS = MODULE.module("biomodeles")
+BIOMODELS = MODULE.module("biomodels")
 
 SEARCH_URL = "https://www.ebi.ac.uk/biomodels/search"
+DOWNLOAD_URL = "https://www.ebi.ac.uk/biomodels/search/download"
+
+
+def parse_sbml(x):
+    return SBMLReader().readSBML(x)
 
 
 def main():
+    """Iterate over COVID-19 models and parse them."""
     models = []
 
     res = requests.get(SEARCH_URL, headers={"Accept": "application/json"}, params={
@@ -37,7 +44,18 @@ def main():
             model["author"] = author
     df = pd.DataFrame(models)
     columns = ["id", "format", "author", "name"]
-    print(tabulate(df[columns], headers=columns, showindex=False))
+    # print(tabulate(df[columns], headers=columns, showindex=False))
+
+    for model_id, model_format in df[["id", "format"]].values:
+        url = f"{DOWNLOAD_URL}?models={model_id}"
+        if model_format == "SBML":
+            # tree = BIOMODELS.ensure_zip_xml(url=url, name=f"{model_id}.zip", inner_path=f"{model_id}.xml")
+            with BIOMODELS.ensure_open_zip(url=url, name=f"{model_id}.zip", inner_path=f"{model_id}.xml") as file:
+                sbml_document = SBMLReader().readSBMLFromString(file.read().decode("utf-8"))
+            print(sbml_document)
+        elif model_format == "COMBINE archive":
+            # with BIOMODELS.ensure_open_zip(url=url, name=f"{model_id}.zip", inner_path=f"{model_id}.omex") as file:
+            continue
 
 
 if __name__ == '__main__':
