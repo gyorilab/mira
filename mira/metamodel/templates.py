f06b1e93187d3c3dc1c2dad971ba59b98a19101b
kkaris <karis.klas@gmail.com>
2022-09-28 10:22:58 -0700

    Fix overflow

--- mira/metamodel/templates.py
+++ mira/metamodel/templates.py
@@ -589,7 +589,10 @@ class TemplateModelDelta:
         node_id = (*template.get_key(), tag)
         node_name = f"{template.type} {tag}"
         self.comparison_graph.add_node(
-            node_id, type=template.type, node_name=node_name, color="yellow" if tag == "1" else "pink"
+            node_id,
+            type=template.type,
+            node_name=node_name,
+            color="yellow" if tag == "1" else "pink",
         )
         return node_id
 
