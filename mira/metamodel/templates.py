1bae8f6ae5ed20619360c6774649d5296e073694
kkaris <karis.klas@gmail.com>
2022-09-29 09:21:47 -0700

    Add RefinementClosure to __all__

--- mira/metamodel/templates.py
+++ mira/metamodel/templates.py
@@ -14,6 +14,7 @@ __all__ = [
     "GroupedControlledConversion",
     "TemplateModel",
     "TemplateModelDelta",
+    "RefinementClosure",
     "get_json_schema",
     "templates_equal",
     "assert_concept_context_refinement",
