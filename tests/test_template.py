43751a89e238708054a8c580f425eb8de25b444b
kkaris <karis.klas@gmail.com>
2022-09-29 17:09:44 -0700

    Update test after template refinement fix

--- tests/test_template.py
+++ tests/test_template.py
@@ -120,6 +120,7 @@ def test_concept_refinement_grounding():
     one_dim_spat_gnd = Concept(
         name="one-dimensional spatial region", identifiers={"bfo": "0000026"}
     )
+    one_dim_spat_gnd_ctx = one_dim_spat_gnd.with_context(location="Stockholm")
     # test grounded
     assert one_dim_spat_gnd.refinement_of(
         spatial_region_gnd, refinement_func=is_ontological_child, with_context=False
@@ -133,16 +134,22 @@ def test_concept_refinement_grounding():
     assert not one_dim_spat_gnd.refinement_of(
         spatial_region, refinement_func=is_ontological_child, with_context=True
     )
+    assert one_dim_spat_gnd_ctx.refinement_of(
+        one_dim_spat_gnd, refinement_func=is_ontological_child, with_context=True
+    )
 
     # test ungrounded
     assert not spatial_region.refinement_of(
         one_dim_spat, refinement_func=is_ontological_child, with_context=False
     )
     spatial_region_ctx = spatial_region.with_context(location="Stockholm")
-    assert spatial_region_ctx.refinement_of(
+    # This one would be True if we consider names when ungrounded. Currently
+    # ungrounded are not considered equal by definition.
+    assert not spatial_region_ctx.refinement_of(
         spatial_region, refinement_func=is_ontological_child, with_context=True
     )
-    assert spatial_region_ctx.refinement_of(
+    #
+    assert not spatial_region_ctx.refinement_of(
         spatial_region_gnd, refinement_func=is_ontological_child, with_context=True
     )
 
