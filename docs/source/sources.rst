e846c9909d7fe8f73a6f3e8c93bdc85fabd83832
Ben Gyori <ben.gyori@gmail.com>
2022-09-30 22:00:49 -0400

    Add to docs

--- docs/source/sources.rst
+++ docs/source/sources.rst
@@ -4,14 +4,20 @@ Sources of model content
     :members:
     :show-inheritance:
 
-BioModels client (:py:mod:`mira.sources.biomodels`)
+Bilayer extraction (:py:mod:`mira.sources.bilayer`)
 ---------------------------------------------------
-.. automodule:: mira.sources.biomodels
+.. automodule:: mira.sources.bilayer
     :members:
     :show-inheritance:
 
 SBML extraction (:py:mod:`mira.sources.sbml`)
 ---------------------------------------------
 .. automodule:: mira.sources.sbml
+    :members:
+    :show-inheritance:
+
+BioModels client (:py:mod:`mira.sources.biomodels`)
+---------------------------------------------------
+.. automodule:: mira.sources.biomodels
     :members:
     :show-inheritance:
--- mira/sources/bilayer.py
+++ mira/sources/bilayer.py
@@ -1,57 +1,39 @@
 """This module implements an input processor for bilayer representations
-of models based on mass-action kinetics.
-
-:: code
-
-    {"Wa":[{"influx":1,"infusion":2},
-                 {"influx":2,"infusion":3}],
-     "Win":[{"arg":1,"call":1},
-                    {"arg":2,"call":1},
-                    {"arg":2,"call":2}],
-     "Box":[{"parameter":"beta"},
-                    {"parameter":"gamma"}],
-     "Qin":[{"variable":"S"},
-                    {"variable":"I"},
-                    {"variable":"R"}],
-     "Qout":[{"tanvar":"S'"},
-                     {"tanvar":"I'"},
-                     {"tanvar":"R'"}],
-     "Wn":[{"efflux":1,"effusion":1},
-                 {"efflux":2,"effusion":2}]}
-"""
+of models based on mass-action kinetics."""
 import json
 from mira.metamodel.templates import *
 
 
-def box_to_template(box):
-    # Assert assumptions in template patterns
-    assert len(box['inputs']) <= 1
-    assert len(box['outputs']) <= 1
-    # Unpack inputs and outputs
-    input = box['inputs'][0] if box['inputs'] else None
-    output = box['outputs'][0] if box['outputs'] else None
-    # Decide on template class based on number of controllers
-    if not box['controllers']:
-        if not input:
-            return NaturalProduction(outcome=output)
-        elif not output:
-            return NaturalDegradation(subject=input)
-        else:
-            return NaturalConversion(subject=input, outcome=output)
-    elif len(box['controllers']) == 1:
-        return ControlledConversion(controller=box['controllers'][0],
-                                    subject=input, outcome=output)
-    else:
-        return GroupedControlledConversion(controllers=box['controllers'],
-                                           subject=input, outcome=output)
+def template_model_from_bilayer_file(fname) -> TemplateModel:
+    """Return a TemplateModel by processing a bilayer JSON file.
 
+    Parameters
+    ----------
+    fname : str
+        The path to a bilayer JSON file.
 
-def template_model_from_bilayer_file(fname):
+    Returns
+    -------
+    :
+        A TemplateModel extracted from the bilayer.
+    """
     with open(fname, 'r') as fh:
         return template_model_from_bilayer(json.load(fname))
 
 
-def template_model_from_bilayer(bilayer_json):
+def template_model_from_bilayer(bilayer_json) -> TemplateModel:
+    """Return a TemplateModel by processing a bilayer JSON file.
+
+    Parameters
+    ----------
+    bilayer_json : dict
+        A bilayer JSON structure.
+
+    Returns
+    -------
+    :
+        A TemplateModel extracted from the bilayer.
+    """
     # For each box
     concepts = {idx + 1: Concept(name=q['variable'])
                 for idx, q in enumerate(bilayer_json['Qin'])}
@@ -72,3 +54,27 @@ def template_model_from_bilayer(bilayer_json):
     for box in boxes:
         templates.append(box_to_template(box))
     return TemplateModel(templates=templates)
+
+
+def box_to_template(box):
+    """Return a Template from a bilayer box by recognizing its topology."""
+    # Assert assumptions in template patterns
+    assert len(box['inputs']) <= 1
+    assert len(box['outputs']) <= 1
+    # Unpack inputs and outputs
+    input = box['inputs'][0] if box['inputs'] else None
+    output = box['outputs'][0] if box['outputs'] else None
+    # Decide on template class based on number of controllers
+    if not box['controllers']:
+        if not input:
+            return NaturalProduction(outcome=output)
+        elif not output:
+            return NaturalDegradation(subject=input)
+        else:
+            return NaturalConversion(subject=input, outcome=output)
+    elif len(box['controllers']) == 1:
+        return ControlledConversion(controller=box['controllers'][0],
+                                    subject=input, outcome=output)
+    else:
+        return GroupedControlledConversion(controllers=box['controllers'],
+                                           subject=input, outcome=output)
