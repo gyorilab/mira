7b641f544ae4da47939c94a115a8eb6148f78ecc
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-05 18:19:05 +0200

    Improve bioregistry hack

--- mira/dkg/__init__.py
+++ mira/dkg/__init__.py
@@ -8,4 +8,20 @@ ASKEMO = bioregistry.Resource(
     description="A custom ontology to support the epidemiology use case in ASKEM.",
     pattern="^\\d{7}$",
 )
-bioregistry.manager.add_resource(ASKEMO)
+
+
+def add_resource(resource: Resource) -> None:
+    """Add a resource to the default registry during current runtime."""
+    manager = bioregistry.manager
+    if resource.prefix in manager.registry:
+        raise KeyError
+    manager.registry[resource.prefix] = resource
+    if resource.has_canonical:
+        manager.canonical_for.setdefault(resource.has_canonical, []).append(resource.prefix)
+    if resource.provides:
+        manager.provided_by.setdefault(resource.provides, []).append(resource.prefix)
+    if resource.part_of:
+        manager.has_parts.setdefault(resource.part_of, []).append(resource.prefix)
+
+
+add_resource(ASKEMO)
