5c9610cd1b219f7e6439d2c7612391446372e23d
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-11-14 13:01:43 +0100

    Add slims to construction

--- mira/dkg/resources/__init__.py
+++ mira/dkg/resources/__init__.py
@@ -1,4 +1,11 @@
 import os
+from pathlib import Path
+
+__all__ = [
+    "HERE",
+    "get_resource_path",
+    "SLIMS",
+]
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -6,3 +13,11 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 def get_resource_path(fname):
     """Return a full path to a given resource file name."""
     return os.path.join(HERE, fname)
+
+
+#: A dictionary of slim (i.e. custom term subset) ontologies
+SLIMS = {
+    "ncit": Path(get_resource_path("ncit_slim.owl")),
+    "covoc": Path(get_resource_path("covoc_slim.owl")),
+    "efo": Path(get_resource_path("efo_slim.owl")),
+}
