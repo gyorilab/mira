10404b14b811eec018a13c7e9b4595e1e83ffff3
kkaris <karis.klas@gmail.com>
2022-12-06 10:54:46 -0800

    Add echo in startup.sh to give feedback on root path

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,7 +3,9 @@ neo4j start
 sleep 100
 neo4j status
 if [ "${ROOT_PATH}" ]; then
+  echo "Running with root path set to ${ROOT_PATH}"
   uvicorn --host 0.0.0.0 --port 8771 mira.dkg.wsgi:app --root-path "${ROOT_PATH}"
 else
+  echo 'No root path set in environment, running at "/"'
   uvicorn --host 0.0.0.0 --port 8771 mira.dkg.wsgi:app
 fi
