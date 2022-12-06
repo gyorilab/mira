43c7ac5274f2155b6b8975f11120f1a2ae1a437d
kkaris <karis.klas@gmail.com>
2022-12-05 16:09:16 -0800

    Add root-path argument to uvicorn startup script

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,4 +2,8 @@
 neo4j start
 sleep 100
 neo4j status
-uvicorn --host 0.0.0.0 --port 8771 mira.dkg.wsgi:app
+if [ "${ROOT_PATH}" ]; then
+  uvicorn --host 0.0.0.0 --port 8771 mira.dkg.wsgi:app --root-path "${ROOT_PATH}"
+else
+  uvicorn --host 0.0.0.0 --port 8771 mira.dkg.wsgi:app
+fi
