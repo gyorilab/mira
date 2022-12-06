ad63ed80f63a06c0953d5b19009e372012e139dd
kkaris <karis.klas@gmail.com>
2022-12-06 09:16:40 -0800

    Allow setting root prefix in environment, add more help text

--- mira/dkg/metaregistry/cli.py
+++ mira/dkg/metaregistry/cli.py
@@ -16,9 +16,12 @@ __all__ = ["main"]
 @click.option("--host", default="0.0.0.0", show_default=True)
 @click.option("--port", default=5000, type=int, show_default=True)
 @click.option("--config", type=Path, help="Path to custom metaregistry configuration.")
-@click.option("--root-path", type=str, help="Set a different root path than "
-                                            "the default, e.g. when running "
-                                            "behind a proxy.")
+@click.option("--root-path", type=str, required=False, default="",
+              help="Set a different root path than the default, e.g. when "
+                   "running behind a proxy. The root path can also be set "
+                   "via the environment using 'ROOT_PATH' for use in e.g. a "
+                   "docker. If both are set, the --root-path option takes "
+                   "precedence over 'ROOT_PATH'.")
 @workers_option
 @with_gunicorn_option
 def main(
@@ -30,6 +33,15 @@ def main(
     workers: int,
 ):
     """Run a custom Bioregistry instance based on a MIRA DKG."""
+    if not root_path:
+        import os
+        if os.environ.get("ROOT_PATH"):
+            root_path = os.environ["ROOT_PATH"]
+            print(f"Using root path {root_path} from environment")
+        else:
+            print(f"No {root_path} set, app's root will be at '/'")
+    else:
+        print(f"Using root path {root_path}")
     app = get_app(config=config, root_prefix=root_path)
     run_app(app, host=host, port=str(port), with_gunicorn=with_gunicorn, workers=workers)
 
