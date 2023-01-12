d9b5b46a2cf4f9a1a21b70b46d231867fc00076d
Ben Gyori <ben.gyori@gmail.com>
2023-01-12 00:58:36 -0500

    Add test to class and fix

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -21,21 +21,22 @@ class TestOperations(unittest.TestCase):
             {template.get_key() for template in actual.templates},
         )
 
-
-def test_simplify_rate_law():
-    template = GroupedControlledConversion(
-        controllers=[
-            Concept('Ailing'),
-            Concept('Diagnosed'),
-            Concept('Infected'),
-            Concept('Recognized')
-        ],
-        subject=Concept('Susceptible'),
-        outcome=Concept('Infected'),
-        rate_law=sympy.parse_expr('1.0*Susceptible*(Ailing*gamma + Diagnosed*beta + Infected*alpha + Recognized*delta)')
-    )
-    simplified_templates = \
-        simplify_rate_law(template, ['alpha', 'beta', 'gamma', 'delta'])
-    assert len(simplified_templates) == 4
-    assert all(isinstance(t, ControlledConversion)
-               for t in simplified_templates)
+    def test_simplify_rate_law(self):
+        parameters = ['alpha', 'beta', 'gamma', 'delta']
+        template = GroupedControlledConversion(
+            controllers=[
+                Concept(name='Ailing'),
+                Concept(name='Diagnosed'),
+                Concept(name='Infected'),
+                Concept(name='Recognized')
+            ],
+            subject=Concept(name='Susceptible'),
+            outcome=Concept(name='Infected'),
+            rate_law=sympy.parse_expr('1.0*Susceptible*(Ailing*gamma + Diagnosed*beta + Infected*alpha + Recognized*delta)',
+                                      local_dict={p: sympy.Symbol(p) for p in parameters})
+        )
+        simplified_templates = \
+            simplify_rate_law(template, parameters)
+        assert len(simplified_templates) == 4
+        assert all(isinstance(t, ControlledConversion)
+                   for t in simplified_templates)
