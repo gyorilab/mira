c9ec4b1c2990227d5dd51675c16217c96489cd9f
Ben Gyori <ben.gyori@gmail.com>
2023-01-12 00:52:04 -0500

    Add test for simplification

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -2,8 +2,12 @@
 
 import unittest
 
+import sympy
+
+from mira.metamodel import Concept, ControlledConversion, \
+    GroupedControlledConversion
 from mira.examples.sir import cities, sir, sir_2_city
-from mira.metamodel.ops import stratify
+from mira.metamodel.ops import stratify, simplify_rate_law
 
 
 class TestOperations(unittest.TestCase):
@@ -16,3 +20,22 @@ class TestOperations(unittest.TestCase):
             {template.get_key() for template in sir_2_city.templates},
             {template.get_key() for template in actual.templates},
         )
+
+
+def test_simplify_rate_law():
+    template = GroupedControlledConversion(
+        controllers=[
+            Concept('Ailing'),
+            Concept('Diagnosed'),
+            Concept('Infected'),
+            Concept('Recognized')
+        ],
+        subject=Concept('Susceptible'),
+        outcome=Concept('Infected'),
+        rate_law=sympy.parse_expr('1.0*Susceptible*(Ailing*gamma + Diagnosed*beta + Infected*alpha + Recognized*delta)')
+    )
+    simplified_templates = \
+        simplify_rate_law(template, ['alpha', 'beta', 'gamma', 'delta'])
+    assert len(simplified_templates) == 4
+    assert all(isinstance(t, ControlledConversion)
+               for t in simplified_templates)
