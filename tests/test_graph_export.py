eaf947536cc3a5f64dd4b3b0bf5b6933025c709d
kkaris <karis.klas@gmail.com>
2023-01-25 11:49:13 -0500

    Test is passing

--- tests/test_graph_export.py
+++ tests/test_graph_export.py
@@ -36,8 +36,68 @@ def test_template_model_comp_graph_export():
 
     # One node per template per TemplateModel + one node per concept per
     # template per TemplateModel
-    template_node_count = len(sir.templates) + len(sir_2_city.templates)
-    concept_node_count = sum(len(t.concepts) for t in sir.templates) + sum(
-        len(t.concepts) for t in sir_2_city.templates
+    template_node_count = len(sir.templates) + len(sir_w_context.templates)
+    assert template_node_count == 4
+
+    concept_keys = set()
+    for t in chain(sir.templates, sir_w_context.templates):
+        for c in t.get_concepts():
+            concept_keys.add(c.get_key())
+
+    concept_node_count = len(concept_keys)
+    assert concept_node_count == 6
+    assert 10 == template_node_count + concept_node_count
+
+    assert len(graph_data.nodes) == template_node_count + concept_node_count
+
+    # One intra edge per concept per template per TemplateModel
+    assert len(graph_data.intra_model_edges) == concept_node_count + template_node_count
+
+    # (One inter edge per refinement + one inter edge per equality) per TemplateModel
+    concept_equal_edges = 0
+    template_equal_edges = 0
+    concept_refinement_edges = 0
+    template_refinement_edges = 0
+    seen_concept_pairs = set()
+    for t1, t2 in product(sir.templates, sir_w_context.templates):
+        if t1.is_equal_to(t2, with_context=True):
+            template_equal_edges += 1
+        if t1.refinement_of(
+            t2, refinement_func=is_ontological_child_web, with_context=True
+        ):
+            template_refinement_edges += 1
+        if t2.refinement_of(
+            t1, refinement_func=is_ontological_child_web, with_context=True
+        ):
+            template_refinement_edges += 1
+        for c1, c2 in product(t1.get_concepts(), t2.get_concepts()):
+            if (c1.get_key(), c2.get_key()) in seen_concept_pairs:
+                continue
+            if c1.is_equal_to(c2, with_context=True):
+                concept_equal_edges += 1
+            if c1.refinement_of(
+                c2, refinement_func=is_ontological_child_web, with_context=True
+            ):
+                concept_refinement_edges += 1
+            if c2.refinement_of(
+                c1, refinement_func=is_ontological_child_web, with_context=True
+            ):
+                concept_refinement_edges += 1
+            seen_concept_pairs.add((c1.get_key(), c2.get_key()))
+
+    # Check that the counts are as expected
+    assert template_equal_edges == 0
+    assert template_refinement_edges == 2
+    assert concept_equal_edges == 0
+    assert concept_refinement_edges == 3
+
+    # check that the total number of inter edges is as expected and
+    # corresponds to what's in the graph data
+    inter_edge_count = (
+        template_equal_edges
+        + template_refinement_edges
+        + concept_refinement_edges
+        + concept_equal_edges
     )
-    assert graph_data.nodes == template_node_count + concept_node_count
+    assert inter_edge_count == 5
+    assert len(graph_data.inter_model_edges) == inter_edge_count
