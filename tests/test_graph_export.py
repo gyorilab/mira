767d85215341ce5b8c97b2660e1edf855cf1392b
kkaris <karis.klas@gmail.com>
2023-01-25 10:45:02 -0500

    Add some initial sanity checks

--- tests/test_graph_export.py
+++ tests/test_graph_export.py
@@ -1,5 +1,7 @@
-from mira.examples.sir import sir, sir_2_city
-from mira.metamodel.templates import TemplateModelComparison
+from itertools import product, chain
+
+from mira.examples.sir import sir
+from mira.metamodel.templates import TemplateModelComparison, TemplateModel
 from mira.dkg.web_client import is_ontological_child_web
 
 
@@ -8,7 +10,9 @@ def test_template_model_comp_graph_export():
     # Check identifications
     # check expected edges
     sir_w_context = TemplateModel(
-        templates=[t.with_context(location="Boston") for t in sir.templates],
+        templates=[
+            t.with_context(location="geonames:4930956") for t in sir.templates
+        ],
         parameters=sir.parameters,
         initials=sir.initials,
     )
@@ -19,6 +23,16 @@ def test_template_model_comp_graph_export():
 
     # Check that the graph export is correct
     graph_data = tmc.model_comparison
+
+    # Check that model ids are integers and non-negative
+    assert len(graph_data.nodes.values()) > 0
+    assert isinstance(list(graph_data.nodes.values())[0].model_id, int)
+    assert list(graph_data.nodes.values())[0].model_id >= 0
+    assert all(isinstance(k, int) for k in graph_data.template_models.keys())
+    assert all(k >= 0 for k in graph_data.template_models.keys())
+    model_id_refs = {k for k in graph_data.template_models.keys()}
+    model_id_refs_nodes = {n.model_id for n in graph_data.nodes.values()}
+    assert model_id_refs == model_id_refs_nodes
 
     # One node per template per TemplateModel + one node per concept per
     # template per TemplateModel
