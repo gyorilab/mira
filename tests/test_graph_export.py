7722bb3341311f7b53ba1050821fdabe7d5962a4
kkaris <karis.klas@gmail.com>
2023-01-26 12:07:57 -0500

    Update test

--- tests/test_graph_export.py
+++ tests/test_graph_export.py
@@ -1,7 +1,8 @@
 from itertools import product, chain
 
 from mira.examples.sir import sir
-from mira.metamodel.templates import TemplateModelComparison, TemplateModel
+from mira.metamodel.templates import TemplateModelComparison, TemplateModel, \
+    Template, Concept
 from mira.dkg.web_client import is_ontological_child_web
 
 
@@ -9,6 +10,9 @@ def test_template_model_comp_graph_export():
     # Check counts
     # Check identifications
     # check expected edges
+
+    # This will create a copy SIR model with context -> should have a
+    # refinement edge between each corresponding pair of nodes in the graph
     sir_w_context = TemplateModel(
         templates=[
             t.with_context(location="geonames:4930956") for t in sir.templates
@@ -26,12 +30,16 @@ def test_template_model_comp_graph_export():
 
     # Check that model ids are integers and non-negative
     assert len(graph_data.nodes.values()) > 0
-    assert isinstance(list(graph_data.nodes.values())[0].model_id, int)
-    assert list(graph_data.nodes.values())[0].model_id >= 0
+    # {model_id: {node_id: Concept|Template}}
+    assert isinstance(list(graph_data.nodes.values())[0], dict)
+    assert isinstance(list(list(graph_data.nodes.values())[0].values())[0],
+                      (Concept, Template))
+    assert list(list(graph_data.nodes.values())[0].keys())[0] >= 0
     assert all(isinstance(k, int) for k in graph_data.template_models.keys())
     assert all(k >= 0 for k in graph_data.template_models.keys())
     model_id_refs = {k for k in graph_data.template_models.keys()}
-    model_id_refs_nodes = {n.model_id for n in graph_data.nodes.values()}
+
+    model_id_refs_nodes = {model_id for model_id in graph_data.nodes.keys()}
     assert model_id_refs == model_id_refs_nodes
 
     # One node per template per TemplateModel + one node per concept per
@@ -48,7 +56,14 @@ def test_template_model_comp_graph_export():
     assert concept_node_count == 6
     assert 10 == template_node_count + concept_node_count
 
-    assert len(graph_data.nodes) == template_node_count + concept_node_count
+    # Check that all models are represented in the node lookup
+    assert len(graph_data.nodes) == len(graph_data.template_models)
+
+    # Check that the total count of nodes is as expected
+    total_count = 0
+    for nodes in graph_data.nodes.values():
+        total_count += len(nodes)
+    assert total_count == template_node_count + concept_node_count
 
     # One intra edge per concept per template per TemplateModel
     assert len(graph_data.intra_model_edges) == concept_node_count + template_node_count
