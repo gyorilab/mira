a059ef85d730e28c160fa85e4dc9f997e5a2b682
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-01-26 11:29:17 -0500

    Update chime control structure

--- mira/examples/chime.py
+++ mira/examples/chime.py
@@ -9,17 +9,38 @@ susceptible = Concept(
 infected = Concept(name="infected_population", identifiers={"ido": "0000511"})
 recovered = Concept(name="recovered", identifiers={"ido": "0000592"})
 
-infection = NaturalConversion(
+infection = ControlledConversion(
     subject=susceptible,
     outcome=infected,
+    controller=infected,
 )
 
 templates = []
-templates.append(infection.with_context(vaccination_status="vaccinated"))
-templates.append(infection.with_context(vaccination_status="unvaccinated"))
+templates.append(
+    infection.with_context(vaccination_status="vaccinated").add_controller(
+        infected.with_context(vaccination_status="unvaccinated")
+    )
+)
+templates.append(
+    infection.with_context(vaccination_status="unvaccinated").add_controller(
+        infected.with_context(vaccination_status="vaccinated")
+    )
+)
 
 for vaccination_status in ["vaccinated", "unvaccinated"]:
     subject = infected.with_context(vaccination_status=vaccination_status)
     templates.append(NaturalConversion(subject=subject, outcome=recovered))
 
 sviivr = TemplateModel(templates=templates)
+
+
+def _main():
+    from mira.modeling.viz import GraphicalModel
+    from pathlib import Path
+
+    gm = GraphicalModel.from_template_model(sviivr)
+    gm.write(Path.home().joinpath("Desktop", "sviivr.png"))
+
+
+if __name__ == "__main__":
+    _main()
