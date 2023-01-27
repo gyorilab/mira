9038f63afae0c57a5010c8b82ae45e397591b2d1
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-01-27 13:25:57 -0500

    Start preparing the scenario 2 svir

--- mira/examples/concepts.py
+++ mira/examples/concepts.py
@@ -3,6 +3,8 @@ from ..metamodel import Concept
 __all__ = [
     "susceptible",
     "infected",
+    "infected_asymptomatic",
+    "infected_symptomatic",
     "recovered",
     "exposed",
     "dead",
@@ -10,6 +12,8 @@ __all__ = [
 
 susceptible = Concept(name="susceptible_population", identifiers={"ido": "0000514"})
 infected = Concept(name="infected_population", identifiers={"ido": "0000511"})
+infected_symptomatic = infected.with_context(status="symptomatic")
+infected_asymptomatic = infected.with_context(status="asymptomatic")
 recovered = Concept(name="immune_population", identifiers={"ido": "0000592"})
 exposed = susceptible.with_context(property="ido:0000597")
 dead = Concept(name="dead", identifiers={"ncit": "C28554"})
