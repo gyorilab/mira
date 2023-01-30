69d110c997b841d683a53664ff5ab8c165f9f002
kkaris <karis.klas@gmail.com>
2023-01-30 13:51:52 -0800

    Add proper identifier for hospitalization

--- mira/examples/concepts.py
+++ mira/examples/concepts.py
@@ -19,5 +19,5 @@ infected_asymptomatic = infected.with_context(status="asymptomatic")
 recovered = Concept(name="immune_population", identifiers={"ido": "0000592"})
 exposed = susceptible.with_context(property="ido:0000597")
 dead = Concept(name="dead", identifiers={"ncit": "C28554"})
-hospitalized = Concept(name="hospitalized")  # FIXME add appropriate grounding
+hospitalized = Concept(name="hospitalized", identifiers={"ncit": "C25179"})
 vaccinated = Concept(name="vaccinated", identifiers={"vo": "0001376"})
