e55cb273a25a0ec723837f17a34afda9e3991497
kkaris <karis.klas@gmail.com>
2023-02-14 14:02:15 -0800

    WIP First iteration of LaTeX table parser

---
+++ mira/sources/space_latex.py
@@ -0,0 +1,87 @@
+import re
+from typing import List
+
+from pandas import DataFrame
+from tqdm import tqdm
+
+
+def parse_sympy_units(latex_str: str):
+    from sympy.parsing.latex import parse_latex
+    # The input is a string of the form:
+    # $ \mathrm{...} \cdot \mathrm{...}^{-<int>} ... $ OR just a single unit
+    # e.g. kg or m or s without the mathmode $...$, find the units and parse
+    # them into a sympy expression
+    pass
+
+
+def parse_table(raw_latex_table: str) -> DataFrame:
+    # Assume this is the text between \begin{tabular} and \end{tabular}
+    # (or 'longtable')
+
+    # Get the rows
+    rows_iter = iter(raw_latex_table.split("\n"))
+
+    # Find the header row, skip the table description, i.e. {|c|c|p{
+    # 2cm}|...} and comments
+    header_row = next(rows_iter)
+    while header_row.strip().startswith(("%", "{", r"\hline")):
+        header_row = next(rows_iter)
+
+    assert "&" in header_row
+
+    # Get the header: it contains LaTeX formatting, like \textbf{...}
+    # Strip whitespace
+    header = [t.strip() for t in header_row.split("&")]
+    # Remove \textbf{...}, \textit{...} and similar formatting
+    header = [re.sub(r"\\textbf\{(.+?)\}", r"\1", t) for t in header]
+    header = [re.sub(r"\\textit\{(.+?)\}", r"\1", t) for t in header]
+
+    # Check if any of the header entries still contain LaTeX formatting
+    # If so, raise an error
+    for t in header:
+        if re.search(r"\\text", t):
+            raise ValueError(f"Header entry '{t}' still contains LaTeX formatting")
+
+    print("Found header:", header)
+
+    # Parse the rows:
+    #   - The Variable column contains LaTeX math
+    #   - The name column is empty 90% of time
+    #   - The Description column contains with the occasional $...$ math
+    #   - The SI-Units column contain LaTeX math describing the
+    #     physical units of the variable/constant in the SI system
+    #   - The Ref. column contains a latex reference to the equation in the
+    #     paper where it was first seen. It's either of the form \ref{eqN} or
+    #     \ref{sami_eqN} (N is the number of the equation). Get N.
+
+    parsed_rows = []
+    for row in rows_iter:
+        # Skip if row does not have correct number of columns
+        columns = tuple(c.strip() for c in row.split("&"))
+        if len(columns) != len(header):
+            continue
+        parsed_rows.append(columns)
+        # todo: Parse the SI-Units column into sympy units
+
+    # Create the DataFrame
+    df = DataFrame(parsed_rows, columns=header)
+    return df
+
+
+def parse_latex_tables(latex_file_path: str) -> List[DataFrame]:
+    """Parse a string containing a LaTeX table into a pandas DataFrame."""
+    # Read the file
+    with open(latex_file_path, "r") as fh:
+        raw_latex = fh.read()
+
+    # Find all tables (also match 'longtable')
+    table_tables = re.findall(r"\\begin{table}(.+?)\\end{table}", raw_latex, re.DOTALL)
+    long_tables = re.findall(r"\\begin{longtable}(.+?)\\end{longtable}", raw_latex, re.DOTALL)
+    tables = table_tables + long_tables
+
+    # Parse each table
+    dfs = []
+    for table in tables:
+        dfs.append(parse_table(table))
+
+    return dfs
