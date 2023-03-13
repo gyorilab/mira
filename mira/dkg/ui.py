f6494bdf71581b619451b51ff8f9c6a850b8914f
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-03-13 16:38:46 +0100

    Remove unused code

--- mira/dkg/ui.py
+++ mira/dkg/ui.py
@@ -12,12 +12,9 @@ ui_blueprint = Blueprint("ui", __name__)
 @ui_blueprint.route("/", methods=["GET"])
 def home():
     """Render the home page."""
-    # key = random.choice(list(grounder.entries))
     return render_template(
         "home.html",
         number_terms=len(grounder.entries),
-        # example_key=key,
-        # example_term=grounder.entries[key][0].to_json(),
         node_counter=client.get_node_counter(),
     )
 
