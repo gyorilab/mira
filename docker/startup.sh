b4d3020e6cb3ae9a9723cd43777753956ce17360
kkaris <karis.klas@gmail.com>
2023-03-16 16:28:53 -0700

    Run indexing after startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,6 +3,11 @@ neo4j start
 sleep 100
 neo4j status
 
+# Index nodes on id property
+python -m mira.dkg.indexing --exist-ok
+
+# Start the service
+
 if [ "${ROOT_PATH}" ]; then
   echo "Running with root path set to ${ROOT_PATH}"
   uvicorn --host 0.0.0.0 --port 8771 mira.dkg.wsgi:app --root-path "${ROOT_PATH}"
