3f5e240b6302337d34224125cfbe9a6fd45e03cf
Ben Gyori <ben.gyori@gmail.com>
2023-05-17 16:31:52 -0400

    Rearrange and extend tests

--- tests/test_askenet_source.py
+++ tests/test_askenet_source.py
@@ -1,13 +1,38 @@
+from mira.metamodel import *
 from mira.sources.askenet import model_from_url
+from mira.sources.askenet import petrinet
+from mira.sources.askenet import regnet
+
+petrinet_example = 'https://raw.githubusercontent.com/DARPA-ASKEM/' \
+    'Model-Representations/main/petrinet/examples/sir.json'
+regnet_example = 'https://raw.githubusercontent.com/DARPA-ASKEM/' \
+    'Model-Representations/main/regnet/examples/lotka_volterra.json'
 
 
-def test_model_from_url():
-    tm = model_from_url('https://raw.githubusercontent.com/DARPA-ASKEM/'
-                        'Model-Representations/main/regnet/examples/'
-                        'lotka_volterra.json')
+def test_petrinet_model_from_url():
+    template_model = petrinet.model_from_url(petrinet_example)
+    assert len(template_model.templates) == 2
+    assert isinstance(template_model.templates[0], ControlledConversion)
+    assert isinstance(template_model.templates[1], NaturalConversion)
+    assert template_model.templates[0].controller.name == 'Infected'
+    assert template_model.templates[0].subject.name == 'Susceptible'
+    assert template_model.templates[0].outcome.name == 'Infected'
+    assert template_model.templates[1].subject.name == 'Infected'
+    assert template_model.templates[1].outcome.name == 'Recovered'
+
+
+def test_regnet_model_from_url():
+    template_model = regnet.model_from_url(regnet_example)
+    assert len(template_model.templates) == 4
+    assert isinstance(template_model.templates[0], ControlledProduction)
+    assert isinstance(template_model.templates[1], NaturalDegradation)
+    assert isinstance(template_model.templates[2], ControlledDegradation)
+    assert isinstance(template_model.templates[3], GroupedControlledProduction)
+
+
+def test_model_from_url_generic():
+    tm = model_from_url(regnet_example)
     assert len(tm.templates) == 4
 
-    tm = model_from_url('https://raw.githubusercontent.com/DARPA-ASKEM/'
-                        'Model-Representations/main/petrinet/examples/'
-                        'sir.json')
+    tm = model_from_url(petrinet_example)
     assert len(tm.templates) == 2
--- tests/test_modeling/test_askenet.py
+++ tests/test_modeling/test_askenet.py
@@ -3,25 +3,6 @@ from mira.examples.concepts import susceptible
 from mira.metamodel import *
 from mira.modeling import Model
 from mira.modeling.askenet.petrinet import AskeNetPetriNetModel
-from mira.sources.askenet.petrinet import (
-    model_from_url,
-    template_model_from_askenet_json,
-)
-
-example = ('https://raw.githubusercontent.com/DARPA-ASKEM/'
-           'Model-Representations/main/petrinet/examples/sir.json')
-
-
-def test_model_from_url():
-    template_model = model_from_url(example)
-    assert len(template_model.templates) == 2
-    assert isinstance(template_model.templates[0], ControlledConversion)
-    assert isinstance(template_model.templates[1], NaturalConversion)
-    assert template_model.templates[0].controller.name == 'Infected'
-    assert template_model.templates[0].subject.name == 'Susceptible'
-    assert template_model.templates[0].outcome.name == 'Infected'
-    assert template_model.templates[1].subject.name == 'Infected'
-    assert template_model.templates[1].outcome.name == 'Recovered'
 
 
 def test_export():
