9bff643d2fe006318d590083879516b4dec594fc
Ben Gyori <ben.gyori@gmail.com>
2023-06-26 15:54:23 -0400

    Add lookup for SBML units

--- mira/resources/__init__.py
+++ mira/resources/__init__.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -17,3 +18,16 @@ def get_resource_file(fname):
         The path to the file.
     """
     return os.path.join(HERE, fname)
+
+
+def make_sbml_units():
+    import libsbml
+    module_contents = dir(libsbml)
+    unit_kinds = {var: var.split('_')[-1].lower()
+                  for var in module_contents
+                  if var.startswith("UNIT_KIND")
+                  and var != "UNIT_KIND_INVALID"}
+    unit_kinds = {getattr(libsbml, var): unit_name
+                  for var, unit_name in unit_kinds.items()}
+    with open(get_resource_file('sbml_units.csv'), 'w') as fh:
+        csv.writer(fh).writerows(sorted(unit_kinds.items()))
