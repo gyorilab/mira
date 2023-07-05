e87de50e4f9cd96140385cf1278198b96ef17e65
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-07-05 15:56:30 +0200

    Fix predicate curie reference

--- mira/dkg/construct.py
+++ mira/dkg/construct.py
@@ -762,7 +762,11 @@ def construct(
                         # intfmt=",",
                     )
                 )
-                edge_counter = Counter(edge.pred for edge in graph.edges)
+                edge_counter = Counter(
+                    edge.predicate.curie
+                    for edge in graph.edges
+                    if edge.predicate is not None
+                )
                 tqdm.write(
                     "\n"
                     + tabulate(
@@ -779,7 +783,7 @@ def construct(
 
             unstandardized_nodes.extend(node.id for node in graph.nodes if not node.reference)
             unstandardized_edges.extend(
-                edge.pred for edge in graph.edges if edge.pred.startswith("http")
+                edge.pred for edge in graph.edges if edge.predicate is None
             )
 
             clean_edges = (
@@ -794,10 +798,10 @@ def construct(
             )
             edges.extend(
                 (
-                    edge.sub,
-                    edge.obj,
-                    _get_edge_name(edge.pred).lower().replace(" ", "_").replace("-", "_"),
-                    edge.pred,
+                    edge.subject.curie,
+                    edge.object.curie,
+                    _get_edge_name(edge.predicate.curie).lower().replace(" ", "_").replace("-", "_"),
+                    edge.predicate.curie,
                     prefix,
                     graph_id,
                     version or "",
