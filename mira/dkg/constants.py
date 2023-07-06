0767e94adaecbed3e37b82c16d561ba396ba8e8e
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-07-06 20:27:43 +0200

    Fix node header

--- mira/dkg/constants.py
+++ mira/dkg/constants.py
@@ -12,6 +12,11 @@ NODE_HEADER = (
     "xrefs:string[]",
     "alts:string[]",
     "version:string",
+    "property_predicates:string[]",
+    "property_values:string[]",
+    "xref_types:string[]",
+    "synonym_types:string[]",
+    "source:string",
 )
 
 #: The used for the edges files in the neo4j bulk import
