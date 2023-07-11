cf9a046e2d67d4c97277b4fa96fc9c58e3e48239
Ben Gyori <ben.gyori@gmail.com>
2023-07-11 00:02:02 -0400

    Remove redundant classes

--- mira/metamodel/templates.py
+++ mira/metamodel/templates.py
@@ -78,51 +78,6 @@ DEFAULT_CONFIG = Config(
     ],
 )
 
-
-<<<<<<< HEAD
-class SympyExprStr(sympy.Expr):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if isinstance(v, cls):
-            return v
-        return cls(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string", example="2*x")
-
-    def __str__(self):
-        return super().__str__()[len(self.__class__.__name__)+1:-1]
-
-    def __repr__(self):
-        return str(self)
-
-
-<<<<<<< HEAD
-class Unit(BaseModel):
-    """A unit of measurement."""
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {
-            SympyExprStr: lambda e: str(e),
-        }
-        json_decoders = {
-            SympyExprStr: lambda e: safe_parse_expr(e)
-        }
-
-    expression: SympyExprStr = Field(
-        description="The expression for the unit."
-    )
-
-
-=======
->>>>>>> 6b3a24e (Reorganize units)
-=======
->>>>>>> 3f8513d (Implement model normalization)
 class Concept(BaseModel):
     """A concept is specified by its identifier(s), name, and - optionally -
     its context.
