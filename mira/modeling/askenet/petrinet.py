8eff2fa0bd15480fb5b0b677d0b289b72c2f3470
Ben Gyori <ben.gyori@gmail.com>
2023-07-13 13:36:08 -0400

    Fix time again

--- mira/modeling/askenet/petrinet.py
+++ mira/modeling/askenet/petrinet.py
@@ -208,7 +208,7 @@ class AskeNetPetriNetModel:
                 'initials': self.initials,
                 'parameters': self.parameters,
                 'observables': self.observables,
-                'time': self.time
+                'time': self.time if self.time else {'id': 't'}
             }},
             'metadata': self.metadata,
         }
@@ -230,7 +230,7 @@ class AskeNetPetriNetModel:
                 initials=[Initial.parse_obj(i) for i in self.initials],
                 parameters=[Parameter.parse_obj(p) for p in self.parameters],
                 observables=[Observable.parse_obj(o) for o in self.observables],
-                time=Time.parse_obj(self.time) if self.time else None
+                time=Time.parse_obj(self.time) if self.time else Time(id='t')
             )),
             metadata=self.metadata,
         )
