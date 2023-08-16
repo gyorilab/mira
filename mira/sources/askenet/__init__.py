ccfbeda48e055965b5ee0056b28e100544310738
Ben Gyori <ben.gyori@gmail.com>
2023-08-16 11:20:08 -0400

    Adapt Askenet source to header

--- mira/sources/askenet/__init__.py
+++ mira/sources/askenet/__init__.py
@@ -23,16 +23,17 @@ def model_from_url(url):
     """
     res = requests.get(url)
     model_json = res.json()
-    if 'schema' not in model_json:
+    header = model_json.get('header', {})
+    if 'schema' not in header:
         raise ValueError(f'No "schema" defined in the AMR at {url}. '
                          f'The schema has to be a URL pointing to a '
                          f'JSON schema against which the AMR is validated.')
-    if 'petrinet' in model_json['schema']:
+    if 'petrinet' in header['schema']:
         return petrinet.template_model_from_askenet_json(model_json)
-    elif 'regnet' in model_json['schema']:
+    elif 'regnet' in header['schema']:
         return regnet.template_model_from_askenet_json(model_json)
     else:
-        raise ValueError(f'Unknown schema: {model_json["schema"]}')
+        raise ValueError(f'Unknown schema: {header["schema"]}')
 
 
 def model_from_json_file(fname):
@@ -50,19 +51,21 @@ def model_from_json_file(fname):
     """
     with open(fname) as fh:
         model_json = json.load(fh)
-    if 'schema' not in model_json:
+    header = model_json.get('header', {})
+    if 'schema' not in header:
         raise ValueError(f'No schema defined in the AMR in {fname}. '
                          f'The schema has to be a URL pointing to a '
                          f'JSON schema against which the AMR is validated.')
-    if 'petrinet' in model_json['schema']:
+    if 'petrinet' in header['schema']:
         return petrinet.template_model_from_askenet_json(model_json)
-    elif 'regnet' in model_json['schema']:
+    elif 'regnet' in header['schema']:
         return regnet.template_model_from_askenet_json(model_json)
     else:
-        raise ValueError(f'Unknown schema: {model_json["schema"]}')
+        raise ValueError(f'Unknown schema: {header["schema"]}')
 
 
 def sanity_check_amr(amr_json):
-    assert "schema" in amr_json
-    schema_json = requests.get(amr_json["schema"]).json()
+    assert 'header' in amr_json
+    assert 'schema' in amr_json['header']
+    schema_json = requests.get(amr_json['header']['schema']).json()
     jsonschema.validate(schema_json, amr_json)
