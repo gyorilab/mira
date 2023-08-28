a4de9b64d5e1434dd04238770463d8aafabb0f84
nanglo123 <tnanglo1998@gmail.com>
2023-08-28 12:15:08 -0400

    Added unit test for replace_parameter_id and changed test_replace_state_id unit test to not check for length equality between old and new amr expressions asstates do not have to be a single letter

--- mira/modeling/askenet/ops.py
+++ mira/modeling/askenet/ops.py
@@ -54,36 +54,32 @@ def replace_transition_id(tm, old_id, new_id):
 
 # As of now, replace_observable_id replaces both 'id' and 'name' field of an observable in output amr
 # can possibly add a new argument for display name and set observable.display_name to new argument and then change
-# tm to json method to set 'name' field of output amr to tm.display_name
+# tm to json method to set 'name' field in new_amr['semantics']['ode']['observables'] to observable.display_name
 @amr_to_mira
 def replace_observable_id(tm, old_id, new_id):
     """Replace the ID of an observable."""
     for obs, observable in copy.deepcopy(tm.observables).items():
         if obs == old_id:
             observable.name = new_id
-
             tm.observables[new_id] = observable
             tm.observables.pop(old_id)
     return tm
 
 
 @amr_to_mira
-# current bug is that it doesn't return the changed parameter
-# expected 2 returned parameters, only got 1 (1 that wasnt changed)
+# current bug is that it doesn't return the changed parameter in new_amr['semantics']['ode']['parameters']
+# expected 2 returned parameters in list of parameters, only got 1 (the 1 that wasn't changed)
 def replace_parameter_id(tm, old_id, new_id):
     """Replace the ID of a parameter."""
-
     for template in tm.templates:
         if template.rate_law:
             template.rate_law = SympyExprStr(
                 template.rate_law.args[0].subs(sympy.Symbol(old_id),
                                                sympy.Symbol(new_id)))
-
     for observable in tm.observables.values():
         observable.expression = SympyExprStr(
             observable.expression.args[0].subs(sympy.Symbol(old_id),
                                                sympy.Symbol(new_id)))
-
     return tm
 
 
--- tests/test_modeling/test_askenet_ops.py
+++ tests/test_modeling/test_askenet_ops.py
@@ -13,40 +13,6 @@ class TestAskenetOperations(unittest.TestCase):
             'https://raw.githubusercontent.com/DARPA-ASKEM/'
             'Model-Representations/main/petrinet/examples/sir.json').json()
 
-    
-
-    # checks for updated id and name field of an observable - suggested change such that we can use a different value
-    # for name rather than reusing new_id
-    def test_replace_observable_id(self):
-        old_id = 'noninf'
-        new_id = 'testinf'
-        amr = _d(self.sir_amr)
-        new_amr = replace_observable_id(amr, old_id, new_id)
-
-        old_semantics_observables = amr['semantics']['ode']['observables']
-        new_semantics_observables = new_amr['semantics']['ode']['observables']
-
-        self.assertEqual(len(old_semantics_observables), len(new_semantics_observables))
-
-        for old_observable, new_observable in zip(old_semantics_observables, new_semantics_observables):
-            if old_observable['id'] == old_id:
-                self.assertEqual(new_observable['id'], new_id) and self.assertEQual(new_observable['name'], new_id)
-
-    def test_replace_transition_id(self):
-        old_id = 'inf'
-        new_id = 'new_inf'
-        amr = _d(self.sir_amr)
-        new_amr = replace_transition_id(amr, old_id, new_id)
-
-        old_model_transitions = amr['model']['transitions']
-        new_model_transitions = new_amr['model']['transitions']
-
-        self.assertEqual(len(old_model_transitions), len(new_model_transitions))
-
-        for old_transitions, new_transition in zip(old_model_transitions, new_model_transitions):
-            if old_transitions['id'] == old_id:
-                self.assertEqual(new_transition['id'], new_id)
-
     def test_replace_state_id(self):
 
         old_id = 'S'
@@ -68,6 +34,7 @@ class TestAskenetOperations(unittest.TestCase):
             if old_state['id'] == old_id:
                 self.assertEqual(new_state['id'], new_id)
 
+        self.assertEqual(len(old_model_transitions), len(new_model_transitions))
         for old_transition, new_transition in zip(old_model_transitions, new_model_transitions):
             if old_id in old_transition['input']:
                 new_value_in_transition_input = new_id in new_transition['input']
@@ -90,23 +57,21 @@ class TestAskenetOperations(unittest.TestCase):
         new_semantics_ode_rates = new_semantics_ode['rates']
 
         # this test doesn't account for if the expression semantic is preserved (e.g. same type of operations)
-        # (e.g. would pass test if we call replace_state_id(I,J) and old expression is "I*X" and new expression is "J+X"
+        # would pass test if we call replace_state_id(I,J) and old expression is "I*X" and new expression is "J+X"
         for old_rate, new_rate in zip(old_semantics_ode_rates, new_semantics_ode_rates):
             if (old_id in old_rate['expression']) or (old_id in old_rate['expression_mathml']):
                 new_value_in_rate_expression = (new_id in new_rate['expression'])
                 old_value_out_rate_expression = (old_id not in new_rate['expression'])
-                equal_length_rates_expression = (len(old_rate['expression']) == len(new_rate['expression']))
-                expression_flag = (
-                        new_value_in_rate_expression and old_value_out_rate_expression and equal_length_rates_expression)
+                expression_flag = (new_value_in_rate_expression and old_value_out_rate_expression)
 
-                new_value_in_rate_math_ml = (new_id in new_rate['expression_mathml'])
-                old_value_out_rate_math_ml = (old_id not in new_rate['expression_mathml'])
-                equal_length_rate_math_ml = (len(old_rate['expression_mathml']) == len(new_rate['expression_mathml']))
-                math_ml_flag = (new_value_in_rate_math_ml and old_value_out_rate_math_ml and equal_length_rate_math_ml)
+                new_value_in_rate_mathml = (new_id in new_rate['expression_mathml'])
+                old_value_out_rate_mathml = (old_id not in new_rate['expression_mathml'])
+                mathml_flag = (new_value_in_rate_mathml and old_value_out_rate_mathml)
 
-                self.assertTrue(expression_flag and math_ml_flag)
+                self.assertTrue(expression_flag and mathml_flag)
 
         # for initials, the dict representing states in the initials list have changed expression values in new_amr
+        # from variables to float values
         old_semantics_ode_initials = old_semantics_ode['initials']
         new_semantics_ode_initials = new_semantics_ode['initials']
 
@@ -120,12 +85,18 @@ class TestAskenetOperations(unittest.TestCase):
         # This is due to initial expressions vs values
         assert len(old_semantics_ode_parameters) == 5
         assert len(new_semantics_ode_parameters) == 2
-        # state id and name for each parameter dict in list of parameters has a '0' appended to it (not 'S' -> 'S0')
-        # so test for equality with 0 and subscript ₀ appended to new state id
-        # name field for each parameter not present in new_amr
+        # state id and name for each parameter dict in list of parameters has a '0' appended to it (not 'S' but 'S0')
+        # so test for equality with 0 and subscript ₀ appended to old state id (assuming that 0 and subscript are
+        # timestamps that will be constantly changing) and then taste if new state id and name field for each
+        # parameter is equal to just state id
+
+        # Currently this test passes no matter what as zip function only iterates over the shorter of two lists
+        # since output only has 2 entries (parameter entries of beta and gamma) as opposed to 5 from
+        # old amr, this test will pass
         for old_params, new_params in zip(old_semantics_ode_parameters, new_semantics_ode_parameters):
             if ((old_id + '0') in old_params['id']) or ((old_id + '₀') in old_params['name']):
-                self.assertEqual(new_id + '0', new_params['id'])
+                self.assertEqual(new_id, new_params['id'])
+                self.assertEqual(new_id, new_params['name'])
 
         old_semantics_ode_observables = old_semantics_ode['observables']
         new_semantics_ode_observables = new_semantics_ode['observables']
@@ -140,12 +111,84 @@ class TestAskenetOperations(unittest.TestCase):
                 old_value_out_observable_expression = (old_id not in new_observable['expression'])
                 expression_flag = (new_value_in_observable_expression and old_value_out_observable_expression)
 
-                new_value_in_observable_math_ml = (new_id in new_observable['expression_mathml'])
-                old_value_out_observable_math_ml = (old_id not in new_observable['expression_mathml'])
-                equal_length_observable_math_ml = (
-                        len(old_observable['expression_mathml']) == len(new_observable['expression_mathml']))
-                math_ml_flag = (
-                        new_value_in_observable_math_ml and old_value_out_observable_math_ml and
-                        equal_length_observable_math_ml)
+                new_value_in_observable_mathml = new_id in new_observable['expression_mathml']
+                old_value_out_observable_mathml = old_id not in new_observable['expression_mathml']
+                mathml_flag = (new_value_in_observable_mathml and old_value_out_observable_mathml)
 
-                self.assertTrue(expression_flag and math_ml_flag)
+                self.assertTrue(expression_flag and mathml_flag)
+
+    def test_replace_transition_id(self):
+        old_id = 'inf'
+        new_id = 'new_inf'
+        amr = _d(self.sir_amr)
+        new_amr = replace_transition_id(amr, old_id, new_id)
+
+        old_model_transitions = amr['model']['transitions']
+        new_model_transitions = new_amr['model']['transitions']
+
+        self.assertEqual(len(old_model_transitions), len(new_model_transitions))
+
+        for old_transitions, new_transition in zip(old_model_transitions, new_model_transitions):
+            if old_transitions['id'] == old_id:
+                self.assertEqual(new_transition['id'], new_id)
+
+    # checks for updated id and name field of an observable - suggested change in method definition
+    # such that we can use a different value for name rather than reusing new_id
+    def test_replace_observable_id(self):
+        old_id = 'noninf'
+        new_id = 'testinf'
+        amr = _d(self.sir_amr)
+        new_amr = replace_observable_id(amr, old_id, new_id)
+
+        old_semantics_observables = amr['semantics']['ode']['observables']
+        new_semantics_observables = new_amr['semantics']['ode']['observables']
+
+        self.assertEqual(len(old_semantics_observables), len(new_semantics_observables))
+
+        for old_observable, new_observable in zip(old_semantics_observables, new_semantics_observables):
+            if old_observable['id'] == old_id:
+                self.assertEqual(new_observable['id'], new_id) and self.assertEqual(new_observable['name'], new_id)
+
+    def test_replace_parameter_id(self):
+        old_id = 'beta'
+        new_id = 'zeta'
+        amr = _d(self.sir_amr)
+        new_amr = replace_parameter_id(amr, old_id, new_id)
+
+        old_semantics_ode_rates = amr['semantics']['ode']['rates']
+        new_semantics_ode_rates = new_amr['semantics']['ode']['rates']
+
+        old_semantics_ode_observables = amr['semantics']['ode']['observables']
+        new_semantics_ode_observables = new_amr['semantics']['ode']['observables']
+
+        self.assertEqual(len(old_semantics_ode_rates), len(new_semantics_ode_rates))
+
+        for old_rate, new_rate in zip(old_semantics_ode_rates, new_semantics_ode_rates):
+            if old_id in old_rate['expression'] and old_id in old_rate['expression_mathml']:
+                new_value_in_rate_expression = new_id in new_rate['expression']
+                old_value_out_rate_expression = old_id not in new_rate['expression']
+
+                expression_flag = (new_value_in_rate_expression and old_value_out_rate_expression)
+
+                new_value_in_rate_mathml = new_id in new_rate['expression_mathml']
+                old_value_out_rate_mathml = old_id not in new_rate['expression_mathml']
+
+                mathml_flag = (new_value_in_rate_mathml and old_value_out_rate_mathml)
+
+                self.assertTrue(expression_flag and mathml_flag)
+
+        for old_observable, new_observable in zip(old_semantics_ode_observables, new_semantics_ode_observables):
+
+            if (old_id in old_observable['states'] and
+                    old_id in old_observable['expression'] and old_id in new_observable['expression_mathml']):
+                new_value_in_observable_expression = new_id in new_observable['expression']
+                old_value_out_observable_expression = old_id not in new_observable['expression']
+
+                expression_flag = (new_value_in_observable_expression and old_value_out_observable_expression)
+
+                new_value_in_observable_mathml = new_id in new_observable['expression_mathml']
+                old_value_out_observable_mathml = old_id not in new_observable['expression_mathml']
+
+                mathml_flag = (new_value_in_observable_mathml and old_value_out_observable_mathml)
+
+                self.assertTrue(expression_flag and mathml_flag)
