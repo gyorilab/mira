916506f25faeb36d5490946fcb9ad18870470eb6
nanglo123 <tnanglo1998@gmail.com>
2023-10-20 16:02:05 -0400

    Started initial implementation of expression tree and created recursive method to find sources of targets in decapode operations

---
+++ mira/metamodel/decapodes.py
@@ -0,0 +1,135 @@
+import requests
+import os
+
+
+class Decapode():
+
+    def __init__(self, var_id, variables, op1_list, op2_list):
+        self.variable_id = var_id
+
+        self.op1_list = op1_list
+        self.op2_list = op2_list
+
+        # find operations that have their result/target as the variable_id
+        self.relevant_op_1 = [op1 for op1 in self.op1_list if op1['tgt'] == self.variable_id]
+        self.relevant_op_2 = [op2 for op2 in self.op2_list if op2['res'] == self.variable_id]
+
+        self.mapping1 = {}
+        self.mapping2 = {}
+
+        self.mapping1[self.variable_id] = []
+        self.mapping2[self.variable_id] = []
+        self.stack = Stack()
+        self.tree = ExpressionTree()
+        if not self.relevant_op_1 and not self.relevant_op_2:
+            return
+
+        self.stack = Stack()
+        self.tree = ExpressionTree()
+
+        # a variable id cannot be the result of multiple operations in op1 or op2 list
+        # go through all the ops that have their target as self.variable_id
+
+        for operator1 in self.relevant_op_1:
+            self.mapping1[self.variable_id].append(operator1)
+
+            # find all operations for unary operations where the src is a tgt
+            unary_op_src = [op1 for op1 in self.op1_list if operator1['src'] == op1['tgt']]
+            for sub_operator1 in unary_op_src:
+                self.find_res_1(sub_operator1['tgt'], sub_operator1['src'], unary_op_src)
+
+        for operation2 in self.relevant_op_2:
+            self.mapping2[self.variable_id].append(operation2)
+
+            # find all binary operations where proj1 and proj2 are the result of binary operations
+
+            binary_op_src_proj1 = [op2 for op2 in self.op2_list if operation2['proj1'] == op2['res']]
+            binary_op_src_proj2 = [op2 for op2 in self.op2_list if operation2['proj2'] == op2['res']]
+
+            for binary_operator_1 in binary_op_src_proj1:
+                self.find_res_2(binary_operator_1['res'], binary_operator_1['proj1'], binary_op_src_proj1)
+                self.find_res_2(binary_operator_1['res'], binary_operator_1['proj2'], binary_op_src_proj1)
+
+            for binary_operator_2 in binary_op_src_proj2:
+                self.find_res_2(binary_operator_2['res'], binary_operator_2['proj1'], binary_op_src_proj2)
+                self.find_res_2(binary_operator_2['res'], binary_operator_2['proj2'], binary_op_src_proj2)
+
+        print()
+
+    # recursive method for identifying unary operator sources
+    def find_res_1(self, parent_var, child_var, rel_op1_list):
+        # base case
+
+        if parent_var not in self.mapping1:
+            self.mapping1[parent_var] = []
+        if not rel_op1_list:
+            return
+        rel_op1_list = [op1 for op1 in self.op1_list if op1['tgt'] == parent_var]
+        for operator1 in rel_op1_list:
+            self.mapping1[parent_var].append(operator1)
+            unary_op_src = [op1 for op1 in self.op1_list if operator1['src'] == op1['tgt']]
+            self.find_res_1(child_var, operator1['src'], unary_op_src)
+
+    # recursion for finding binary operator sources
+    def find_res_2(self, parent_var, child_var, rel_op2_list):
+        if parent_var not in self.mapping2:
+            self.mapping2[parent_var] = []
+        if not rel_op2_list:
+            return
+        rel_op2_list = [op2 for op2 in self.op2_list if op2['res'] == parent_var]
+        for operator2 in rel_op2_list:
+
+            if operator2 not in self.mapping2[parent_var]:
+                self.mapping2[parent_var].append(operator2)
+
+            # find all binary operations where proj1 and proj2 are the result of binary operations
+            src_proj1 = [op2 for op2 in self.op2_list if operator2['proj1'] == op2['res']]
+            src_proj2 = [op2 for op2 in self.op2_list if operator2['proj2'] == op2['res']]
+            self.find_res_2(child_var, operator2['proj1'], src_proj1)
+            self.find_res_2(child_var, operator2['proj2'], src_proj2)
+
+
+class ExpressionTree:
+    def inorder(self, x):
+        if not x:
+            return
+        self.inorder(x.left)
+        print(x.value, end=" ")
+        self.inorder(x.right)
+
+
+class Node:
+    def __init__(self, value=None, left=None, right=None, next=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, node):
+        if not self.head:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+
+    def pop(self):
+        if self.head:
+            popped = self.head
+            self.head = self.head.next
+            return popped
+        else:
+            raise Exception("Stack is empty")
+
+
+def main():
+    decapode = requests.get(
+        "https://raw.githubusercontent.com/ciemss/Decapodes.jl/sa_climate_modeling/examples/climate/ice_dynamics.json").json()
+
+
+if __name__ == "__main__":
+    main()
