44d61a8f59cf0de4a2b7a3f0d92a493e6f07dfdb
nanglo123 <tnanglo1998@gmail.com>
2023-10-24 14:07:59 -0400

    Started support for expression trees, fixed bugs for recursively finding srcs for unary and binary operators

--- mira/metamodel/decapodes.py
+++ mira/metamodel/decapodes.py
@@ -1,6 +1,8 @@
 import requests
 import os
 
+test = None
+
 
 class Decapode():
 
@@ -17,33 +19,24 @@ class Decapode():
 
         self.mapping1 = {}
         self.mapping2 = {}
-
         self.mapping1[self.variable_id] = []
         self.mapping2[self.variable_id] = []
-        self.stack = Stack()
+
         self.tree = ExpressionTree()
         self.linked_list = LinkedList()
-        self.expression = ''
 
         if not self.relevant_op_1 and not self.relevant_op_2:
             return
-
-        self.stack = Stack()
-        self.tree = ExpressionTree()
 
         # a variable id cannot be the result of multiple operations in op1 or op2 list
         # go through all the ops that have their target as self.variable_id
 
         for operation1 in self.relevant_op_1:
-            self.mapping1[self.variable_id].append(operation1)
-
             # find all operations for unary operations where the src is a tgt
-            unary_op_src = [op1 for op1 in self.op1_list if operation1['src'] == op1['tgt']]
-            for src_operation1 in unary_op_src:
-                self.find_res1(self.variable_id, src_operation1['src'], self.relevant_op_1)
+            self.find_res1(self.variable_id, operation1['src'], self.relevant_op_1)
 
         for operation2 in self.relevant_op_2:
-            self.mapping2[self.variable_id].append(operation2)
+            # find all operations for binary operations where proj1 is a res and where proj2 is a res
             self.find_res2(self.variable_id, operation2['proj1'], self.relevant_op_2)
             self.find_res2(self.variable_id, operation2['proj2'], self.relevant_op_2)
 
@@ -56,11 +49,12 @@ class Decapode():
             self.mapping1[child_var] = []
         if not rel_op1_list:
             return
-        # rel_op1_list = [op1 for op1 in self.op1_list if op1['tgt'] == parent_var]
         for operator1 in rel_op1_list:
-            self.mapping1[parent_var].append(operator1)
+            if operator1 not in self.mapping1[parent_var]:
+                self.mapping1[parent_var].append(operator1)
             unary_op_src = [op1 for op1 in self.op1_list if operator1['src'] == op1['tgt']]
-            self.find_res1(child_var, operator1['src'], unary_op_src)
+            for unary_operator in unary_op_src:
+                self.find_res1(unary_operator['tgt'], unary_operator['src'], unary_op_src)
 
     # recursion for finding binary operator sources
     def find_res2(self, parent_var, child_var, rel_op2_list):
@@ -86,12 +80,22 @@ class Decapode():
                 self.find_res2(binary_operator_2['res'], binary_operator_2['proj1'], src_proj2)
                 self.find_res2(binary_operator_2['res'], binary_operator_2['proj2'], src_proj2)
 
-
     def create_unary_linked_list(self):
         for var_id, operator_list in self.mapping1.items():
             for operator in operator_list:
                 if operator['src'] in self.mapping1:
                     self.linked_list.insert_end(operator['src'], operator['op1'])
+
+    def insert_all(self, var_id, operator):
+        if not self.mapping2[var_id]:
+            return
+
+        self.tree.insert(operator[0]['op2'])
+        self.tree.insert(operator[0]['proj1'])
+        self.tree.insert(operator[0]['proj2'])
+
+        self.insert_all(operator[0]['proj1'], self.mapping2[operator[0]['proj1']])
+        self.insert_all(operator[0]['proj2'], self.mapping2[operator[0]['proj2']])
 
 
 class LinkedListNode:
@@ -118,40 +122,102 @@ class LinkedList:
         current_node.next = new_node
 
 
+def is_operator(s):
+    s = str(s)
+    if '+' in s:
+        return True
+    if '-' in s:
+        return True
+    if '*' in s:
+        return True
+    if '/' in s:
+        return True
+    if '^' in s:
+        return True
+    return False
+
+
 class ExpressionTree:
     def __init__(self):
-        self.Tree = list()
-        self.Fringe = list()
-        self.Root = 0
+        self.Tree = []
+        for index in range(1, 30):
+            self.Tree.append(TreeNode(left_child_index=index))
+        self.fringe = []
+        self.root = 0
+        self.next_free_child = 0
+
+    def insert(self, new_token):
+        if self.next_free_child == -1:  # check if tree is full
+            return "Tree Full"
+            # tree is not full, safe to insert new token
+        if self.next_free_child == 0:
+            self.Tree[self.root].variable_id = new_token
+            self.next_free_child = self.Tree[self.root].left_child
+            self.Tree[self.root].left_child = -1
+        else:
+            # insert into tree with existing nodes
+            # starting with Root
+            current = 0  # index of the current node
+            previous = -1  # index of previous node
+            new_node = self.Tree[self.next_free_child]  # declare new node
+            new_node.variable_id = new_token
+            # Finding the node at which the NewNode can be inserted
+            while current != -1:
+                curr_node = self.Tree[current]
+                # check if CurrNode contains an operator
+                if is_operator(curr_node.variable_id):
+                    # if LeftChild is empty, insert here
+                    if curr_node.left_child == -1:
+                        curr_node.left_child = self.next_free_child
+                        self.next_free_child = new_node.left_child
+                        new_node.left_child = -1
+                        current = -1
+                    # if RightChild is empty, insert here
+                    elif curr_node.right_child == -1:
+                        curr_node.right_child = self.next_free_child
+                        self.next_free_child = new_node.left_child
+                        new_node.left_child = -1
+                        current = -1
+                    # if LeftChild is an operator
+                    # traverse LeftChild subtree
+                    elif is_operator(self.Tree[curr_node.left_child].variable_id):
+                        previous = current
+                        current = curr_node.left_child
+                        self.fringe.append(previous)
+                    # if RightChild is an operator
+                    # traverse RightChild subtree
+                    elif is_operator(self.Tree[curr_node.right_child].variable_id):
+                        previous = current
+                        current = curr_node.right_child
+                        self.fringe.append(previous)
+                    # traverse right subtree
+                    else:
+                        previous = self.fringe.pop(-1)
+                        current = self.Tree[previous].right_child
+                # no place to insert
+                else:
+                    return "Cannot be inserted"
+
+    def display(self):
+        for index in range(len(self.Tree)):
+            print("Index: ", index, "| Variable ID: ", self.Tree[index].variable_id)
+
+    def infix(self, root, arr):
+        if root.variable_id is not None:
+            if is_operator(root.variable_id):
+                arr.append('(')
+            self.infix(self.Tree[root.left_child], arr)
+            arr.append(root.DataValue)
+            self.infix(self.Tree[root.right_child], arr)
+            if is_operator(root.variable_id):
+                arr.append(')')
 
 
 class TreeNode:
-    def __init__(self, var_id=None, proj1_id=None, proj2_id=None, operator=None, next=None):
-        self.variable_id = var_id
-        self.proj1_id = proj1_id
-        self.proj2_id = proj2_id
-        self.operator = operator
-        self.next = next
-
-
-class Stack:
-    def __init__(self):
-        self.head = None
-
-    def push(self, node):
-        if not self.head:
-            self.head = node
-        else:
-            node.next = self.head
-            self.head = node
-
-    def pop(self):
-        if self.head:
-            popped = self.head
-            self.head = self.head.next
-            return popped
-        else:
-            raise Exception("Stack is empty")
+    def __init__(self, left_child_index, variable_id=None):
+        self.variable_id = None
+        self.left_child = left_child_index
+        self.right_child = -1
 
 
 def main():
