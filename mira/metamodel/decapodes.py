d7b3ef01d0b3f56bab389de19cc01830cfb0f5bd
nanglo123 <tnanglo1998@gmail.com>
2023-10-26 10:44:34 -0400

    Start implementing expression for a variable in decapode

--- mira/metamodel/decapodes.py
+++ mira/metamodel/decapodes.py
@@ -1,17 +1,18 @@
 import requests
 import os
+import sympy
+from collections import deque
 
-test = None
 
+class Variable():
+    def __init__(self, var_id, type, name, op1_list=None, op2_list=None):
 
-class Decapode():
-
-    def __init__(self, var_id, op1_list, op2_list, variables=None):
         self.variable_id = var_id
+        self.type = type
+        self.name = name
 
         self.op1_list = op1_list
         self.op2_list = op2_list
-        self.nodes = {}
 
         # find operations that have their result/target as the variable_id
         self.relevant_op_1 = [op1 for op1 in self.op1_list if op1['tgt'] == self.variable_id]
@@ -24,6 +25,8 @@ class Decapode():
 
         self.tree = ExpressionTree()
         self.linked_list = LinkedList()
+        #
+        self.expression = ''
 
         if not self.relevant_op_1 and not self.relevant_op_2:
             return
@@ -40,8 +43,13 @@ class Decapode():
             self.find_res2(self.variable_id, operation2['proj1'], self.relevant_op_2)
             self.find_res2(self.variable_id, operation2['proj2'], self.relevant_op_2)
 
-    # recursive method for identifying unary operator sources
+    def __repr__(self):
+        return self.name
 
+    def __str__(self):
+        return self.__repr__()
+
+    # recursive method for identifying unary operator sources
     def find_res1(self, parent_var, child_var, rel_op1_list):
         if parent_var not in self.mapping1:
             self.mapping1[parent_var] = []
@@ -96,6 +104,62 @@ class Decapode():
 
         self.insert_all(operator[0]['proj1'], self.mapping2[operator[0]['proj1']])
         self.insert_all(operator[0]['proj2'], self.mapping2[operator[0]['proj2']])
+
+    def create_expression(self, var_id, var_map, parent_var=None, proj_1=None):
+
+        if not self.mapping2[var_id]:
+            return
+
+        if not parent_var:
+            self.expression += '(proj1:' + var_map[self.mapping2[var_id][0]['proj1']] + '| operator:' + \
+                               self.mapping2[var_id][0][
+                                   'op2'] + '| proj_2:' + var_map[self.mapping2[var_id][0]['proj2']] + ' )|'
+        elif parent_var and proj_1:
+            self.expression += '(parent_var:' + var_map[self.mapping2[parent_var][0]['proj1']] + '= proj1:' + var_map[
+                self.mapping2[var_id][0]['proj1']] + '| operator:' + self.mapping2[var_id][0]['op2'] + '| proj_2:' + \
+                               var_map[self.mapping2[var_id][0]['proj2']] + ' )|'
+        elif parent_var and not proj_1:
+            self.expression += '(parent_var:' + var_map[self.mapping2[parent_var][0]['proj2']] + '= proj1:' + var_map[
+                self.mapping2[var_id][0]['proj1']] + '| operator:' + self.mapping2[var_id][0]['op2'] + '| proj_2:' + \
+                               var_map[self.mapping2[var_id][0]['proj2']] + ' )|'
+
+        self.create_expression(self.mapping2[var_id][0]['proj1'], var_map, var_id, True)
+        self.create_expression(self.mapping2[var_id][0]['proj2'], var_map, var_id, False)
+
+    def create_expression_2nd(self, parent_id,var_id, var_input_map, expression, o):
+
+        if var_id not in var_input_map:
+            pass
+        elif var_id in var_input_map:
+            var_input_map[parent_id] = var_input_map[var_id]
+
+
+
+class Op1:
+    def __init__(self, src, tgt, op1):
+        self.src = src
+        self.tgt = tgt
+        self.op1 = op1
+
+    def __repr__(self):
+        return f'Op1({self.src}, {self.tgt}, {self.op1})'
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class Op2:
+    def __init__(self, proj1, proj2, res, op2):
+        self.proj1 = proj1
+        self.proj2 = proj2
+        self.res = res
+        self.op2 = op2
+
+    def __repr__(self):
+        return f'Op2({self.proj1}, {self.proj2}, {self.res}, {self.op2})'
+
+    def __str__(self):
+        return self.__repr__()
 
 
 class LinkedListNode:
@@ -211,6 +275,90 @@ class ExpressionTree:
             self.infix(self.Tree[root.right_child], arr)
             if is_operator(root.variable_id):
                 arr.append(')')
+
+    def infix_to_postfix(self, infix_input: list) -> list:
+        """
+        Converts infix expression to postfix.
+        Args:
+            infix_input(list): infix expression user entered
+        """
+
+        # precedence order and associativity helps to determine which
+        # expression is needs to be calculated first
+        precedence_order = {'+': 0, '-': 0, '*': 1, '/': 1, '^': 2}
+        associativity = {'+': "LR", '-': "LR", '*': "LR", '/': "LR", '^': "RL"}
+        # clean the infix expression
+        clean_infix = self._clean_input(infix_input)
+
+        i = 0
+        postfix = []
+        operators = "+-/*^"
+        stack = deque()
+        while i < len(clean_infix):
+
+            char = clean_infix[i]
+            # print(f"char: {char}")
+            # check if char is operator
+            if char in operators:
+                # check if the stack is empty or the top element is '('
+                if len(stack) == 0 or stack[0] == '(':
+                    # just push the operator into stack
+                    stack.appendleft(char)
+                    i += 1
+                # otherwise compare the curr char with top of the element
+                else:
+                    # peek the top element
+                    top_element = stack[0]
+                    # check for precedence
+                    # if they have equal precedence
+                    if precedence_order[char] == precedence_order[top_element]:
+                        # check for associativity
+                        if associativity[char] == "LR":
+                            # pop the top of the stack and add to the postfix
+                            popped_element = stack.popleft()
+                            postfix.append(popped_element)
+                        # if associativity of char is Right to left
+                        elif associativity[char] == "RL":
+                            # push the new operator to the stack
+                            stack.appendleft(char)
+                            i += 1
+                    elif precedence_order[char] > precedence_order[top_element]:
+                        # push the char into stack
+                        stack.appendleft(char)
+                        i += 1
+                    elif precedence_order[char] < precedence_order[top_element]:
+                        # pop the top element
+                        popped_element = stack.popleft()
+                        postfix.append(popped_element)
+            elif char == '(':
+                # add it to the stack
+                stack.appendleft(char)
+                i += 1
+            elif char == ')':
+                top_element = stack[0]
+                while top_element != '(':
+                    popped_element = stack.popleft()
+                    postfix.append(popped_element)
+                    # update the top element
+                    top_element = stack[0]
+                # now we pop opening parenthases and discard it
+                stack.popleft()
+                i += 1
+            # char is operand
+            else:
+                postfix.append(char)
+                i += 1
+            #     print(postfix)
+            # print(f"stack: {stack}")
+
+        # empty the stack
+        if len(stack) > 0:
+            for i in range(len(stack)):
+                postfix.append(stack.popleft())
+        # while len(stack) > 0:
+        #     postfix.append(stack.popleft())
+
+        return postfix
 
 
 class TreeNode:
