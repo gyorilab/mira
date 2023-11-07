ab4dc8f2d86dc42e37d80676d85be2a6b3bd88a3
kkaris <karis.klas@gmail.com>
2023-11-07 10:30:40 -0800

    Add tests for decaexpr processing

---
+++ tests/test_decexpr.py
@@ -0,0 +1,9 @@
+from mira.examples.decapodes.decapodes_examples import *
+
+
+def test_oscillator_decaexpr():
+    get_oscillator_decaexpr()
+
+
+def test_friction_decaexpr():
+    get_friction_decaexpr()
