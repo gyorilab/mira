e376278a3cca1ca1213c3e624f1ed346add9f17d
kkaris <karis.klas@gmail.com>
2023-11-07 10:33:29 -0800

    Add tests for decapode processing

---
+++ tests/test_decapodes.py
@@ -0,0 +1,12 @@
+from mira.examples.decapodes.decapodes_examples import (
+    get_oscillator_decapode,
+    get_friction_decapode
+)
+
+
+def test_oscillator_decapode():
+    get_oscillator_decapode()
+
+
+def test_friction_decapode():
+    get_friction_decapode()
