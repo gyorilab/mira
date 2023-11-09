3c4ca379a59d1c57c4bb94b6617571292b5287d7
kkaris <karis.klas@gmail.com>
2023-11-09 00:23:56 -0800

    Remove unused mappings in util.py

--- mira/sources/acsets/decapodes/util.py
+++ mira/sources/acsets/decapodes/util.py
@@ -1,7 +1,1 @@
 PARTIAL_TIME_DERIVATIVE = "∂ₜ"
-BINARY_OPERATIONS = {"Mult"}  # todo: extend
-FUNCTION_NAME_MAPPING = {"♯": 'Sharp',
-                         '⋆₁': 'dot_subscript_1',
-                         '∂ₜ': "d_subscript_t",
-                         '⋆₀⁻¹': 'dot_subscript_o_superscript_-1'}
-INVERSE_FUNCTION_NAME_MAPPING = {v: k for k, v in FUNCTION_NAME_MAPPING.items()}
