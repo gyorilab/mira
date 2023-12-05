38e11685bb138af4f2f27a30672e283d769e9da0
kkaris <karis.klas@gmail.com>
2023-12-05 14:57:30 -0800

    Add/extend docstrings for helpers in decapodes_examples.py

--- mira/examples/decapodes/decapodes_examples.py
+++ mira/examples/decapodes/decapodes_examples.py
@@ -20,48 +20,101 @@ DECAEXPR_FRICTION = EXAMPLES / "friction_decaexpr.json"
 
 
 def get_ice_dynamics_example() -> Decapode:
-    """Return the ice dynamics decapode example"""
+    """Return the ice dynamics decapode example
+
+    Returns
+    -------
+    :
+        The ice dynamics example as a Decapode object
+    """
     res_json = requests.get(ICE_DYNAMICS_EXAMPLE_JSON_URL).json()
     return process_decapode(res_json)
 
 
 def get_ice_decapode_json():
+    """Return the ice dynamics decapode example as json
+
+    Returns
+    -------
+    : JSON
+        The ice dynamics example as a json object
+    """
     return requests.get(ICE_DYNAMICS_EXAMPLE_JSON_URL).json()
 
 
 def get_oscillator_decapode() -> Decapode:
-    """Return the oscillator decapode example"""
+    """Return the oscillator decapode example
+
+    Example from
+
+    Returns
+    -------
+    :
+        The oscillator example as a Decapode object
+    """
     with open(DECAPODE_OSCILLATOR) as f:
         decapode_osc_json = json.load(f)
         return process_decapode(decapode_osc_json)
 
 
 def get_oscillator_decapode_json():
+    """Return the oscillator decapode example as json
+
+    Returns
+    -------
+    : JSON
+        The oscillator example as a json object
+    """
     with open(DECAPODE_OSCILLATOR) as f:
         return json.load(f)
 
 
 def get_friction_decapode() -> Decapode:
-    """Return the friction decaexpr example"""
+    """Return the friction decaexpr example
+
+    Returns
+    -------
+    :
+        The friction example as a Decapode object
+    """
     with open(DECAPODE_FRICTION) as f:
         decapode_friction_json = json.load(f)
         return process_decapode(decapode_friction_json)
 
 
 def get_friction_decapode_json():
+    """Return the friction decaexpr example as json
+
+    Returns
+    -------
+    : JSON
+        The friction example as a json object
+    """
     with open(DECAPODE_FRICTION) as f:
         return json.load(f)
 
 
 def get_oscillator_decaexpr() -> Decapode:
-    """Return the oscillator decaexpr example"""
+    """Return the oscillator decaexpr example
+
+    Returns
+    -------
+    :
+        The oscillator example as a Decapode object
+    """
     with open(DECAEXPR_OSCILLATOR) as f:
         decaexpr_osc_json = json.load(f)
         return process_decaexpr(decaexpr_osc_json)
 
 
 def get_friction_decaexpr() -> Decapode:
-    """Return the friction decaexpr example"""
+    """Return the friction decaexpr example
+
+    Returns
+    -------
+    :
+        The friction example as a Decapode object
+    """
     with open(DECAEXPR_FRICTION) as f:
         decaexpr_friction_json = json.load(f)
         return process_decaexpr(decaexpr_friction_json)
