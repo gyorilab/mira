5527dc645102fce0ef510294536454307c02b2db
nanglo123 <tnanglo1998@gmail.com>
2023-12-21 20:54:36 -0500

    Create initial implementation of vensim digestion with while loop

--- mira/sources/vensim/__init__.py
+++ mira/sources/vensim/__init__.py
@@ -1,21 +1,106 @@
 import requests
 import io
+import sympy
+from mira.metamodel.utils import safe_parse_expr
 
 
-class MDL:
-    def __init__(self):
-        pass
+STOP_CHARACTER = "\\\\\\---///Sketchinformation-donotmodifyanythingexceptnames"
+CONTROL_DELIMETER = "********************************************************"
+INTEG_FUNCTION = sympy.Function("INTEG")
+SYMBOL_MAP = {"alpha": sympy.Symbol("α"), "beta": sympy.Symbol("β")}
+CONTROL_VARIABLE_NAMES = {"FINALTIME", "INITIALTIME", "SAVEPER", "TIMESTEP"}
+
+SEIR_URL = "https://metasd.com/wp-content/uploads/2020/03/SEIR-SS-growth3.mdl"
+CHEWING_URL = "https://metasd.com/wp-content/uploads/2020/03/chewing-1.mdl"
 
 
-# mld based off model described here: https://metasd.com/2020/03/steady-state-growth-sir-seir-models/
-URL = "https://metasd.com/wp-content/uploads/2020/03/SEIR-SS-growth3.mdl"
+def process_bytes_to_str(byte_object):
+    return (
+        byte_object.decode()
+        .replace("\r\n", "")
+        .replace("\t", "")
+        .replace(" ", "")
+        .replace('"', "")
+        .replace("&", "and")
+    )
+
+
+def parse_mdl_file(url):
+    data = requests.get(url).content
+    byte_stream = io.BytesIO(data)
+    byte_list = byte_stream.readlines()
+    text_list = list(map(process_bytes_to_str, byte_list))
+    var_dict = {}
+    i = 0
+
+    while i < len(text_list):
+        text = text_list[i]
+        # lines beginning with ~ are comments
+        if text == STOP_CHARACTER:
+            break
+        # The declaration of control variables is always 6 lines below the control delimiter
+        if text == CONTROL_DELIMETER:
+            i += 6
+            continue
+        if "|" in text or "~" in text or "{UTF-8}" in text or not text:
+            i += 1
+            continue
+
+        # regular variable
+        if (
+            text[len(text) - 1] == "="
+            and text[:-1] not in CONTROL_VARIABLE_NAMES
+        ):
+            var_name = text[:-1]
+            # Some variable names have operators in the name and g*'s expression involves variables
+            # so it's not possible to parse with safe_parse_expr
+            if "g*" in var_name or "/" in var_name or "*" in var_name:
+                i += 2
+                continue
+
+            var_dict[var_name] = {"name": var_name}
+
+            expression = safe_parse_expr(text_list[i + 1], SYMBOL_MAP)
+            var_dict[var_name]["expression"] = expression
+            i += 2
+
+        # variable with "INTEG" operator that performs integral, it's declaration is multi-line
+        elif "INTEG" in text:
+            var_integ_declaration = text.split("=")
+            var_name = var_integ_declaration[0]
+            var_dict[var_name] = {"name": var_name}
+
+            expression = INTEG_FUNCTION(
+                text_list[i + 1][:-1], text_list[i + 2][:-1]
+            )
+            var_dict[var_name]["expression"] = expression
+            i += 3
+
+        # control variables
+        else:
+            if "SAVEPER" not in text:
+                control_var_declaration = text.split("=")
+                var_name = control_var_declaration[0]
+                value = control_var_declaration[1]
+
+                var_dict[var_name] = {"name": var_name}
+                var_dict[var_name]["value"] = value
+
+                i += 1
+
+            # When we come across "SAVEPER", just skip it as it's value is set to "TIMESTEP"
+            # however, "SAVEPER" is initialized before "TIMESTEP"
+            else:
+                i += 2
+
+    # Add "SAVEPER" to var_dict here
+    var_dict["SAVEPER"] = {
+        "name": "SAVEPER",
+        "value": var_dict["TIMESTEP"]["value"],
+    }
+    return var_dict
+
+
 if __name__ == "__main__":
-    data = requests.get(URL).content
-
-    f = io.BytesIO(data)
-
-    l = f.readlines()
-
-    for k in l:
-        print(k)
-        print()
+    l = parse_mdl_file(CHEWING_URL)
+    print()
