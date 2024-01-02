4908159b1590091711fcc6b6731e0860e2cdeb20
nanglo123 <tnanglo1998@gmail.com>
2024-01-02 15:31:08 -0500

    Handle variable names that cannot be parsed by sympy

--- mira/sources/vensim/__init__.py
+++ mira/sources/vensim/__init__.py
@@ -10,8 +10,12 @@ from mira.metamodel.utils import safe_parse_expr
 STOP_CHARACTER = "\\\\\\---///Sketchinformation-donotmodifyanythingexceptnames"
 CONTROL_DELIMETER = "********************************************************"
 INTEG_FUNCTION = sympy.Function("INTEG")
-SYMBOL_MAP = {"alpha": sympy.Symbol("α"), "beta": sympy.Symbol("β")}
+SYMBOL_MAP = {
+    "alpha": sympy.Symbol("α"),
+    "beta": sympy.Symbol("β"),
+}
 CONTROL_VARIABLE_NAMES = {"FINALTIME", "INITIALTIME", "SAVEPER", "TIMESTEP"}
+CONVERTED_VAR_NAME_MAP = {}
 
 SEIR_URL = "https://metasd.com/wp-content/uploads/2020/03/SEIR-SS-growth3.mdl"
 CHEWING_URL = "https://metasd.com/wp-content/uploads/2020/03/chewing-1.mdl"
@@ -41,6 +45,57 @@ def process_str(text):
     )
 
 
+def is_content_text(text):
+    if "|" in text or "~" in text or "{UTF-8}" in text or not text:
+        return False
+    else:
+        return True
+
+
+def convert_expression_text(old_expression_text):
+    new_expression_text = old_expression_text
+    for old_var_name, new_var_name in CONVERTED_VAR_NAME_MAP.items():
+        if old_var_name != new_var_name:
+            new_expression_text = new_expression_text.replace(
+                old_var_name, new_var_name
+            )
+    return new_expression_text.replace("^", "**")
+
+
+# modifies the list parameter, doesn't return anything
+def create_converted_variable_name_mapping(text_list):
+    i = 0
+    while i < len(text_list):
+        text = text_list[i]
+        if text == STOP_CHARACTER:
+            break
+
+        if not is_content_text(text):
+            i += 1
+            continue
+
+        if (
+            text[len(text) - 1] == "="
+            and text[:-1] not in CONTROL_VARIABLE_NAMES
+        ):
+            old_var_name = text[:-1]
+            new_var_name = old_var_name.replace("*", "_star_").replace(
+                "/", "_fslash_"
+            )
+            CONVERTED_VAR_NAME_MAP[old_var_name] = new_var_name
+            text_list[i] = new_var_name + "="
+            if "\\" in text_list[i + 1]:
+                i += 3
+                continue
+            i += 2
+            continue
+        elif "INTEG" in text:
+            i += 3
+            continue
+        elif text == CONTROL_DELIMETER:
+            break
+
+
 def parse_mdl_file(url_or_path, is_url=True):
     text_list = []
     if is_url:
@@ -54,6 +109,7 @@ def parse_mdl_file(url_or_path, is_url=True):
                 text_list.append(line)
         text_list = list(map(process_str, text_list))
 
+    create_converted_variable_name_mapping(text_list)
     var_dict = {}
     i = 0
 
@@ -70,7 +126,7 @@ def parse_mdl_file(url_or_path, is_url=True):
         # first line is always the encoding
         # skip past empty strings
         # TODO: Handle multi-line comments as the non-initial comment lines do not contain "~"
-        if "|" in text or "~" in text or "~" or "{UTF-8}" in text or not text:
+        if not is_content_text(text):
             i += 1
             continue
 
@@ -80,16 +136,9 @@ def parse_mdl_file(url_or_path, is_url=True):
             and text[:-1] not in CONTROL_VARIABLE_NAMES
         ):
             var_name = text[:-1]
-            # TODO: Create a first-pass implementation that gathers all variable names and
-            #  convert them to names that are parseable by sympy
-
-            # this case specifically pertains to the seir model
-            if "g*" in var_name or "/" in var_name or "*" in var_name:
-                i += 2
-                continue
-
             var_dict[var_name] = {"name": var_name}
 
+            # Handle expressions for variables that span 2 lines
             if "\\" in text_list[i + 1]:
                 expression = safe_parse_expr(
                     text_list[i + 1][:-1] + text_list[i + 2], SYMBOL_MAP
@@ -97,7 +146,8 @@ def parse_mdl_file(url_or_path, is_url=True):
                 var_dict[var_name]["expression"] = expression
                 i += 3
                 continue
-            expression = safe_parse_expr(text_list[i + 1], SYMBOL_MAP)
+            expression_text = convert_expression_text(text_list[i + 1])
+            expression = safe_parse_expr(expression_text, SYMBOL_MAP)
             var_dict[var_name]["expression"] = expression
             i += 2
 
@@ -131,13 +181,16 @@ def parse_mdl_file(url_or_path, is_url=True):
                 i += 2
 
     # Add "SAVEPER" to var_dict here
+    print()
     var_dict["SAVEPER"] = {
         "name": "SAVEPER",
         "value": var_dict["TIMESTEP"]["value"],
     }
+
     return var_dict
 
 
 if __name__ == "__main__":
-    j = parse_mdl_file(CORONOA_FILE_NAME, is_url=False)
-    print()
+    j = parse_mdl_file(SEIR_URL, is_url=True)
+    
+
