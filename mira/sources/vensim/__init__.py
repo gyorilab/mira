a77e08325b5c399bc9025780560aa10cb0ea305e
nanglo123 <tnanglo1998@gmail.com>
2024-01-05 15:51:07 -0500

    Create template model from information extract from pysd except for templates

--- mira/sources/vensim/__init__.py
+++ mira/sources/vensim/__init__.py
@@ -1,26 +1,28 @@
-import io
 from pathlib import Path
 
 import pysd
+from pysd.translators.vensim.vensim_file import VensimFile
 import requests
 import sympy
-import re
 
 from mira.metamodel import *
 from mira.metamodel.utils import safe_parse_expr
 from mira.metamodel import Concept, TemplateModel
-from mira.sources.util import parameter_to_mira
-
+from mira.sources.util import parameter_to_mira, transition_to_templates
 
 STOP_CHARACTER = "\\\\\\---///Sketchinformation-donotmodifyanythingexceptnames"
 CONTROL_DELIMETER = "********************************************************"
-INTEG_FUNCTION = sympy.Function("INTEG")
+NEW_CONTROL_DELIMETER = (
+    " ******************************************************** .Control "
+    "********************************************************"
+)
+
 SYMBOL_MAP = {
     "alpha": sympy.Symbol("α"),
     "beta": sympy.Symbol("β"),
 }
 CONTROL_VARIABLE_NAMES = {"FINALTIME", "INITIALTIME", "SAVEPER", "TIMESTEP"}
-CONVERTED_VAR_NAME_MAP = {}
+INTEG_FUNCTION = sympy.Function("Integ")
 
 SEIR_URL = "https://metasd.com/wp-content/uploads/2020/03/SEIR-SS-growth3.mdl"
 CHEWING_URL = "https://metasd.com/wp-content/uploads/2020/03/chewing-1.mdl"
@@ -35,193 +37,198 @@ SEIR_PATH = EXAMPLE_DIRECTORY / "seir-growth.mdl"
 
 # If we are retrieving the contents of a mdl file hosted online, decode the content in bytes to
 # strings
-def process_bytes_to_str(byte_object):
-    return (
-        byte_object.decode()
-        .replace("\r\n", "")
-        .replace("\t", "")
-        .replace(" ", "")
-        .replace('"', "")
-        .replace("&", "and")
-    )
-
-
-# Process each string in the file if we are loading a mdl file
-def process_str(text):
-    return (
-        text.replace("\r\n", "")
-        .replace("\t", "")
-        .replace(" ", "")
-        .replace('"', "")
-        .replace("&", "and")
-        .replace("\n", "")
-    )
-
-
-# test to see if the current string contains variable information or metadata
-def is_content_text(text):
-    if "|" in text or "~" in text or "{UTF-8}" in text or not text:
-        return False
-    else:
-        return True
-
-
-# converts the expression of a variable into sympy parseable strings
-def convert_expression_text(old_expression_text):
-    new_expression_text = old_expression_text
-    for old_var_name, new_var_name in CONVERTED_VAR_NAME_MAP.items():
-        if old_var_name != new_var_name:
-            new_expression_text = new_expression_text.replace(
-                old_var_name, new_var_name
-            )
-    return new_expression_text.replace("^", "**")
-
-
-# modifies the list parameter and returns it
-def create_converted_variable_name_mapping(text_list):
-    i = 0
-    while i < len(text_list):
-        text = text_list[i]
-        if text == STOP_CHARACTER:
-            break
-
-        if not is_content_text(text):
-            i += 1
-            if "\\" in text_list[i]:
-                while text_list[i] != "|":
-                    i += 1
-            continue
-
-        if (
-            text[len(text) - 1] == "="
-            and text[:-1] not in CONTROL_VARIABLE_NAMES
-        ):
-            old_var_name = text[:-1]
-            new_var_name = old_var_name.replace("*", "_star_").replace(
-                "/", "_fslash_"
-            )
-            CONVERTED_VAR_NAME_MAP[old_var_name] = new_var_name
-            text_list[i] = new_var_name + "="
-
-            if "\\" in text_list[i + 1]:
-                i += 1
-                while "~" not in text_list[i]:
-                    i += 1
-                continue
-            i += 2
-            continue
-        elif "INTEG" in text:
-            i += 3
-            continue
-        elif text == CONTROL_DELIMETER:
-            break
-    return text_list
-
-
-def parse_mdl_file(url_or_path, is_url=True):
-    text_list = []
-    if is_url:
-        data = requests.get(url_or_path).content
-        byte_stream = io.BytesIO(data)
-        byte_list = byte_stream.readlines()
-        text_list = list(map(process_bytes_to_str, byte_list))
-    else:
-        with open(url_or_path) as f:
-            for line in f:
-                text_list.append(line)
-        text_list = list(map(process_str, text_list))
-
-    text_list = create_converted_variable_name_mapping(text_list)
-    var_dict = {}
-    i = 0
-
-    while i < len(text_list):
-        text = text_list[i]
-        if text == STOP_CHARACTER:
-            break
-        # The declaration of control variables is always 6 lines below the control delimiter
-        if text == CONTROL_DELIMETER:
-            i += 6
-            continue
-
-        # lines beginning with "~" are comments
-        # lines that have a "|" are a delimiter for variable declaration
-        # first line is always the encoding
-        # skip past empty strings
-        if not is_content_text(text):
-            i += 1
-
-            # handles multi-line comments
-            if "\\" in text_list[i] and text_list[i] != STOP_CHARACTER:
-                while text_list[i] != "|":
-                    i += 1
-            continue
-
-        # TODO:Handle lookups and input series data, not highest priority
-        # regular variable
-        if (
-            text[len(text) - 1] == "="
-            and text[:-1] not in CONTROL_VARIABLE_NAMES
-        ):
-            var_name = text[:-1]
-            var_dict[var_name] = {"name": var_name}
-
-            # Handle expressions for variables that span multiple lines
-            if "\\" in text_list[i + 1]:
-                i += 1
-                built_expression_text = ""
-                while "~" not in text_list[i]:
-                    built_expression_text += text_list[i].replace("\\", "")
-                    i += 1
-
-                expression = safe_parse_expr(
-                    convert_expression_text(built_expression_text), SYMBOL_MAP
-                )
-                var_dict[var_name]["expression"] = expression
-                continue
-
-            # Handle expressions for variables defined on one line
-            expression_text = convert_expression_text(text_list[i + 1])
-            expression = safe_parse_expr(expression_text, SYMBOL_MAP)
-            var_dict[var_name]["expression"] = expression
-            i += 2
-
-        # variable with "INTEG" operator that performs integral, its declaration is multi-line
-        elif "INTEG" in text:
-            var_integ_declaration = text.split("=")
-            var_name = var_integ_declaration[0]
-            var_dict[var_name] = {"name": var_name}
-
-            expression = INTEG_FUNCTION(
-                text_list[i + 1][:-1], text_list[i + 2][:-1]
-            )
-            var_dict[var_name]["expression"] = expression
-            i += 3
-
-        # control variables
-        else:
-            if "SAVEPER" not in text:
-                control_var_declaration = text.split("=")
-                var_name = control_var_declaration[0]
-                value = control_var_declaration[1]
-
-                var_dict[var_name] = {"name": var_name}
-                var_dict[var_name]["value"] = value
-
-                i += 1
-
-            # When we come across "SAVEPER", skip it and go to the next declared control variable
-            # as it's value is set to "TIMESTEP", however; "SAVEPER" is initialized before
-            # "TIMESTEP"
-            else:
-                i += 6
-
-    # Add "SAVEPER" to var_dict here
-    var_dict["SAVEPER"] = {
-        "name": "SAVEPER",
-        "value": var_dict["TIMESTEP"]["value"],
-    }
-    return var_dict
+# def process_bytes_to_str(byte_object):
+#     return (
+#         byte_object.decode()
+#         .replace("\r\n", "")
+#         .replace("\t", "")
+#         .replace(" ", "")
+#         .replace('"', "")
+#         .replace("&", "and")
+#     )
+#
+#
+# # Process each string in the file if we are loading a mdl file
+# def process_str(text):
+#     return (
+#         text.replace("\r\n", "")
+#         .replace("\t", "")
+#         .replace(" ", "")
+#         .replace('"', "")
+#         .replace("&", "and")
+#         .replace("\n", "")
+#     )
+#
+#
+# # test to see if the current string contains variable information or metadata
+# def is_content_text(text):
+#     if "|" in text or "~" in text or "{UTF-8}" in text or not text:
+#         return False
+#     else:
+#         return True
+#
+#
+# # converts the expression of a variable into sympy parseable strings
+# def convert_expression_text(old_expression_text, converted_var_name_map):
+#     for old_var_name, new_var_name in converted_var_name_map.items():
+#         if old_var_name != new_var_name:
+#             old_expression_text = old_expression_text.replace(
+#                 old_var_name, new_var_name
+#             )
+#     return old_expression_text.replace("^", "**")
+#
+#
+# #
+# # # modifies the list parameter and returns it
+# def create_converted_variable_name_mapping(text_list):
+#     convereted_var_name_map = {}
+#     i = 0
+#     while i < len(text_list):
+#         text = text_list[i]
+#         if text == STOP_CHARACTER:
+#             break
+#
+#         if not is_content_text(text):
+#             i += 1
+#             if "\\" in text_list[i]:
+#                 while text_list[i] != "|":
+#                     i += 1
+#             continue
+#
+#         if (
+#             text[len(text) - 1] == "="
+#             and text[:-1] not in CONTROL_VARIABLE_NAMES
+#         ):
+#             old_var_name = text[:-1]
+#             new_var_name = old_var_name.replace("*", "_star_").replace(
+#                 "/", "_fslash_"
+#             )
+#             convereted_var_name_map[old_var_name] = new_var_name
+#             text_list[i] = new_var_name + "="
+#
+#             if "\\" in text_list[i + 1]:
+#                 i += 1
+#                 while "~" not in text_list[i]:
+#                     i += 1
+#                 continue
+#             i += 2
+#             continue
+#         elif "INTEG" in text:
+#             i += 3
+#             continue
+#         elif text == CONTROL_DELIMETER:
+#             break
+#     return text_list, convereted_var_name_map
+#
+#
+# def parse_mdl_file(url_or_path, is_url=True):
+#     text_list = []
+#     if is_url:
+#         data = requests.get(url_or_path).content
+#         byte_stream = io.BytesIO(data)
+#         byte_list = byte_stream.readlines()
+#         text_list = list(map(process_bytes_to_str, byte_list))
+#     else:
+#         with open(url_or_path) as f:
+#             for line in f:
+#                 text_list.append(line)
+#         text_list = list(map(process_str, text_list))
+#
+#     text_list, converted_ver_name_map = create_converted_variable_name_mapping(
+#         text_list
+#     )
+#     var_dict = {}
+#     i = 0
+#
+#     while i < len(text_list):
+#         text = text_list[i]
+#         if text == STOP_CHARACTER:
+#             break
+#         # The declaration of control variables is always 6 lines below the control delimiter
+#         if text == CONTROL_DELIMETER:
+#             i += 6
+#             continue
+#
+#         # lines beginning with "~" are comments
+#         # lines that have a "|" are a delimiter for variable declaration
+#         # first line is always the encoding
+#         # skip past empty strings
+#         if not is_content_text(text):
+#             i += 1
+#
+#             # handles multi-line comments
+#             if "\\" in text_list[i] and text_list[i] != STOP_CHARACTER:
+#                 while text_list[i] != "|":
+#                     i += 1
+#             continue
+#
+#         # TODO:Handle lookups and input series data, not highest priority
+#         # regular variable
+#         if (
+#             text[len(text) - 1] == "="
+#             and text[:-1] not in CONTROL_VARIABLE_NAMES
+#         ):
+#             var_name = text[:-1]
+#             var_dict[var_name] = {"name": var_name}
+#
+#             # Handle expressions for variables that span multiple lines
+#             if "\\" in text_list[i + 1]:
+#                 i += 1
+#                 built_expression_text = ""
+#                 while "~" not in text_list[i]:
+#                     built_expression_text += text_list[i].replace("\\", "")
+#                     i += 1
+#
+#                 expression = safe_parse_expr(
+#                     convert_expression_text(built_expression_text), SYMBOL_MAP
+#                 )
+#                 var_dict[var_name]["expression"] = expression
+#                 continue
+#
+#             # Handle expressions for variables defined on one line
+#             expression_text = convert_expression_text(
+#                 text_list[i + 1], converted_ver_name_map
+#             )
+#             expression = safe_parse_expr(expression_text, SYMBOL_MAP)
+#             var_dict[var_name]["expression"] = expression
+#             i += 2
+#
+#         # variable with "INTEG" operator that performs integral, its declaration is multi-line
+#         elif "INTEG" in text:
+#             var_integ_declaration = text.split("=")
+#             var_name = var_integ_declaration[0]
+#             var_dict[var_name] = {"name": var_name}
+#
+#             expression = INTEG_FUNCTION(
+#                 text_list[i + 1][:-1], text_list[i + 2][:-1]
+#             )
+#             var_dict[var_name]["expression"] = expression
+#             i += 3
+#
+#         # control variables
+#         else:
+#             if "SAVEPER" not in text:
+#                 control_var_declaration = text.split("=")
+#                 var_name = control_var_declaration[0]
+#                 value = control_var_declaration[1]
+#
+#                 var_dict[var_name] = {"name": var_name}
+#                 var_dict[var_name]["value"] = value
+#
+#                 i += 1
+#
+#             # When we come across "SAVEPER", skip it and go to the next declared control variable
+#             # as it's value is set to "TIMESTEP", however; "SAVEPER" is initialized before
+#             # "TIMESTEP"
+#             else:
+#                 i += 6
+#
+#     # Add "SAVEPER" to var_dict here
+#     var_dict["SAVEPER"] = {
+#         "name": "SAVEPER",
+#         "value": var_dict["TIMESTEP"]["value"],
+#     }
+#     return var_dict
 
 
 def state_to_concept(state) -> Concept:
@@ -233,22 +240,52 @@ def state_to_concept(state) -> Concept:
     return Concept(name=name, units=units_obj, description=description)
 
 
-def template_model_from_mdl_file(
-    file_path, var_dict, *, url=None
-) -> TemplateModel:
+def template_model_from_mdl_file(file_path, *, url=None) -> TemplateModel:
     if url:
         data = requests.get(url).content
         with open(file_path, "wb") as file:
             file.write(data)
+
+    utf_encoding = "{UTF-8} "
+
+    vensim_file = VensimFile(file_path)
+    model_split_text = vensim_file.model_text.split("|")
     model = pysd.read_vensim(file_path)
     model_doc_df = model.doc
-    states = model_doc_df[model_doc_df["Type"] == "Stateful"]
 
+    old_var_name_map = dict(
+        zip(model_doc_df["Real Name"], model_doc_df["Py Name"])
+    )
+
+    # for real_name, py_name in old_var_name_map.items():
+    #     new_real_name = real_name.replace(" ", "").replace('"', "")
+    #     # Add what will appear in old_expression to py_name to Symbol Mapping
+    #     SYMBOL_MAP[new_real_name] = sympy.Symbol(py_name)
+
+    # map from new var names to sympy expression
+    new_var_expression_map = {}
+    for text in model_split_text:
+        if NEW_CONTROL_DELIMETER in text:
+            break
+        if "=" not in text:
+            continue
+        if utf_encoding in text:
+            text = text.replace(utf_encoding, "")
+        var_declaration = text.split("~")[0].split("=")
+        old_var_name = var_declaration[0].strip()
+        old_text_expression = var_declaration[1]
+        # old_text_expression = (
+        #     var_declaration[1].replace(" ", "").replace('"', "")
+        # )
+        # new_sympy_expression = safe_parse_expr(old_text_expression, SYMBOL_MAP)
+        new_var_expression_map[old_var_name_map[old_var_name]] = old_text_expression
+
+
+    states = model_doc_df[model_doc_df["Type"] == "Stateful"]
     mira_states = {}
     all_states = set()
     symbols = {}
-
-    # process stocks
+    # process states
     # identifiers and context missing
     for index, state in states.iterrows():
         concept_state = state_to_concept(state)
@@ -258,18 +295,13 @@ def template_model_from_mdl_file(
 
     # process parameters
     mira_parameters = {}
-    for var in var_dict.values():
-        parameter_sympy_value = var.get("expression")
-        if (
-            type(parameter_sympy_value) is sympy.Integer
-            or type(parameter_sympy_value) is sympy.Float
-        ):
-            name = re.sub(r"(\w)([A-Z])", r"\1_\2", var.get("name")).lower()
+    for name, expression in new_var_expression_map.items():
+        #  Variables whose values are only numeric are Mira Parameters
+        if expression.replace(".", "").isnumeric():
             model_parameter_info = model_doc_df[model_doc_df["Py Name"] == name]
-
             parameter = {
                 "id": name,
-                "value": float(parameter_sympy_value),
+                "value": float(expression),
                 "description": model_parameter_info["Comment"].values[0],
                 "units": {
                     "expression": model_parameter_info["Units"].values[0]
@@ -278,6 +310,7 @@ def template_model_from_mdl_file(
             mira_parameters[name] = parameter_to_mira(parameter)
 
     # process initials
+    # currently we just retrieve initials based on states
     mira_initials = {}
     for state in mira_states.values():
         try:
@@ -289,25 +322,50 @@ def template_model_from_mdl_file(
         except TypeError:
             continue
 
-    # process observables
+    # No observable or readout section
     mira_observables = {}
 
     # process transitions
     used_states = set()
     templates_ = []
-
     auxiliaries = model_doc_df[model_doc_df["Type"] == "Auxiliary"]
-    for index, aux in auxiliaries.iterrows():
+    for template_id, aux_tuple in enumerate(auxiliaries.iterrows()):
+        aux = aux_tuple[1]
         if (
             aux["Subtype"] == "Normal"
             and aux["Real Name"] not in CONTROL_VARIABLE_NAMES
         ):
-            name = aux["Real Name"].strip()
-            expression = var_dict.get(name).get("expression")
+            if aux["Real Name"] == "Recovering":
+                input_concepts = [mira_states["infectious"]]
+                output_concepts = [mira_states["recovered"]]
+                controller_concepts = []
+                rate_expr = safe_parse_expr(
+                    new_var_expression_map[aux["Py Name"]], SYMBOL_MAP
+                )
 
-    return model
+                templates_.extend(
+                    transition_to_templates(
+                        input_concepts,
+                        output_concepts,
+                        controller_concepts,
+                        rate_expr,
+                        template_id + 1,
+                        aux["Real Name"],
+                    )
+                )
+
+    tm_description = model_split_text[0].split("~")[1]
+    anns = Annotations(descriptin=tm_description)
+
+    return TemplateModel(
+        templates=templates_,
+        parameters=mira_parameters,
+        initials=mira_initials,
+        observable=mira_observables,
+        annotations=anns,
+    )
 
 
 if __name__ == "__main__":
-    seir_variables = parse_mdl_file(SIR_PATH, is_url=False)
-    m1 = template_model_from_mdl_file(SIR_PATH, seir_variables)
+    tm_sir = template_model_from_mdl_file(SIR_PATH, url=SIR_URL)
+    print()
