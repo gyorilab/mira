d2fcbd9625529b6cc0d92273f46b83548967c962
Ben Gyori <ben.gyori@gmail.com>
2024-04-26 15:28:10 -0400

    Use fewer models from web for testing

--- tests/test_sbml_qual.py
+++ tests/test_sbml_qual.py
@@ -7,30 +7,7 @@ import requests
 from mira.sources.sbml.qual_api import template_model_from_sbml_qual_string
 from mira.sources.biomodels import get_sbml_model
 
-models = [
-    "Apoptosis",
-    "Coagulation-pathway",
-    "ER_Stress",
-    "ETC",
-    "E_protein",
-    "HMOX1_Pathway",
-    "IFN-lambda",
-    "Interferon1",
-    "JNK_pathway",
-    "Kynurenine_pathway",
-    "NLRP3_Activation",
-    "Nsp14",
-    "Nsp4_Nsp6",
-    "Nsp9_protein",
-    "Orf10_Cul2_pathway",
-    "Orf3a",
-    "PAMP_signaling",
-    "Pyrimidine_deprivation",
-    "RTC-and-transcription",
-    "Renin_angiotensin",
-    "TGFB_pathway",
-    "Virus_replication_cycle",
-]
+models = ["Apoptosis"]
 BASE_URL = "https://git-r3lab.uni.lu/covid/models/-/raw/master/Executable%20Modules/SBML_qual_build/sbml"
 
 
@@ -42,7 +19,9 @@ def test_qual_models_from_example_repo():
 
 
 def test_qual_models_from_biomodels():
-    model_ids = ["BIOMD0000000562", "BIOMD0000000592", "BIOMD0000000593"]
+    model_ids = ["BIOMD0000000562"]
+    # Full model list for reference
+    #model_ids = ["BIOMD0000000562", "BIOMD0000000592", "BIOMD0000000593"]
     for model_id in model_ids:
         model_text = get_sbml_model(model_id)
         tm = template_model_from_sbml_qual_string(model_text)
