993b9050779d399697cc734d426ee1d17894a7f9
nanglo123 <tnanglo1998@gmail.com>
2024-09-19 10:21:37 -0400

    Format file

--- docker/generate_obo_graphs.py
+++ docker/generate_obo_graphs.py
@@ -5,6 +5,7 @@ from obonet import read_obo
 import networkx
 import pickle
 
+
 def download_convert_ncbitaxon_obo_to_graph():
     resource_prefix = "ncbitaxon"
     version = get_version(resource_prefix)
@@ -12,17 +13,20 @@ def download_convert_ncbitaxon_obo_to_graph():
     # Checks to see if the pickled ncbitaxon obo graph exists in the container
     cached_relabeled_obo_graph_path = Path("/graphs/ncbitaxon_obo_graph.pkl")
     if not cached_relabeled_obo_graph_path.exists():
-        _, obo_path = _ensure_ontology_path(resource_prefix, force=False,
-                                            version=version)
+        _, obo_path = _ensure_ontology_path(
+            resource_prefix, force=False, version=version
+        )
         obo_graph = read_obo(obo_path)
 
         # Normalize node indices
-        relabeled_graph = networkx.relabel_nodes(obo_graph,
-                                                 lambda node_index:
-                                                 node_index.lower())
-        with open(cached_relabeled_obo_graph_path,
-                  'wb') as relabeled_graph_file:
+        relabeled_graph = networkx.relabel_nodes(
+            obo_graph, lambda node_index: node_index.lower()
+        )
+        with open(
+            cached_relabeled_obo_graph_path, "wb"
+        ) as relabeled_graph_file:
             pickle.dump(relabeled_graph, relabeled_graph_file)
+
 
 if __name__ == "__main__":
     download_convert_ncbitaxon_obo_to_graph()
