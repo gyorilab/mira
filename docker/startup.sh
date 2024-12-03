da0b769e24358de7ab08b33cfc6a4e6598b8760b
kkaris <karis.klas@gmail.com>
2024-12-03 13:50:18 -0700

    Probe db instead of sleeping 100 s

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+set -eoxu pipefail
+
+echo "Starting database"
 neo4j start
-sleep 100
+
+echo "Waiting for database"
+until [ \
+  "$(curl -s -w '%{http_code}' -o /dev/null "http://localhost:7474")" \
+  -eq 200 ]
+do
+  sleep 5
+done
+
 neo4j status
 
 # Index nodes on id property
