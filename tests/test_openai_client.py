ad09405e7891a3828a090d18887fc5a72bf5ae28
kkaris <karis.klas@gmail.com>
2024-12-04 16:46:12 -0700

    Format file

--- tests/test_openai_client.py
+++ tests/test_openai_client.py
@@ -8,17 +8,17 @@ import requests
 from mira.openai import OpenAIClient
 
 
-@unittest.skipIf(os.environ.get('GITHUB_ACTIONS') is not None,
 HERE = Path(__file__).parent.resolve()
 
 
+@unittest.skipIf(os.environ.get("GITHUB_ACTIONS") is not None,
                  reason="Meant to be run locally")
-@unittest.skipIf(os.environ.get('OPENAI_API_KEY') is None,
+@unittest.skipIf(os.environ.get("OPENAI_API_KEY") is None,
                  reason="Need OPENAI_API_KEY environment variable to run")
 def test_explain_image():
     bananas_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Cavendish_Banana_DS.jpg/640px-Cavendish_Banana_DS.jpg"
     res = requests.get(bananas_url)
-    base64_image = base64.b64encode(res.content).decode('utf-8')
+    base64_image = base64.b64encode(res.content).decode("utf-8")
 
     client = OpenAIClient()
     response = client.run_chat_completion_with_image(
