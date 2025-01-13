3a73c3f1190a019f663fc189daf586d8a3b7ba3b
kkaris <karis.klas@gmail.com>
2025-01-13 14:50:36 -0800

    Add and complete docstrings

--- mira/sources/sympy_ode/llm_util.py
+++ mira/sources/sympy_ode/llm_util.py
@@ -18,6 +18,21 @@ def image_file_to_odes_str(
     image_path: str,
     client: OpenAIClient,
 ) -> str:
+    """Get an ODE string from an image file depicting an ODE system
+
+    Parameters
+    ----------
+    image_path :
+        The path to the image file
+    client :
+        A :class:`mira.openai.OpenAIClient` instance
+
+    Returns
+    -------
+    :
+        The ODE string extracted from the image. The string should contain the code
+        necessary to define the ODEs using sympy.
+    """
     with open(image_path, "rb") as f:
         image_bytes = f.read()
     image_format = image_path.split(".")[-1]
@@ -29,6 +44,23 @@ def image_to_odes_str(
     client: OpenAIClient,
     image_format: ImageFmts = "png"
 ) -> str:
+    """Get an ODE string from an image depicting an ODE system
+
+    Parameters
+    ----------
+    image_bytes :
+        The bytes of the image
+    client :
+        The OpenAI client
+    image_format :
+        The format of the image. The default is "png".
+
+    Returns
+    -------
+    :
+        The ODE string extracted from the image. The string should contain the code
+        necessary to define the ODEs using sympy.
+    """
     base64_image = base64.b64encode(image_bytes).decode('utf-8')
     response = extract_ode_str_from_base64_image(base64_image=base64_image,
                                                  image_format=image_format,
@@ -37,6 +69,20 @@ def image_to_odes_str(
 
 
 def clean_response(response: str) -> str:
+    """Clean up the response from the OpenAI chat completion
+
+    Parameters
+    ----------
+    response :
+        The response from the OpenAI chat completion.
+
+    Returns
+    -------
+    :
+        The cleaned up response. The response is stripped of the code block
+        markdown, i.e. the triple backticks and the language specifier. It is also
+        stripped of leading and trailing whitespaces.
+    """
     response = response.replace("```python", "")
     response = response.replace("```", "")
     return response.strip()
@@ -48,6 +94,26 @@ def extract_ode_str_from_base64_image(
     client: OpenAIClient,
     prompt: str = None
 ):
+    """Get the ODE string from an image in base64 format
+
+    Parameters
+    ----------
+    base64_image :
+        The base64 encoded image
+    image_format :
+        The format of the image
+    client :
+        The OpenAI client
+    prompt :
+        The prompt to send to the OpenAI chat completion. If None, the default
+        prompt is used (see :data:`mira.sources.sympy_ode.constants.ODE_IMAGE_PROMPT`)
+
+    Returns
+    -------
+    :
+        The ODE string extracted from the image. The string should contain the code
+        necessary to define the ODEs using sympy.
+    """
     if prompt is None:
         prompt = ODE_IMAGE_PROMPT
 
@@ -64,7 +130,7 @@ def get_concepts_from_odes(
     ode_str: str,
     client: OpenAIClient,
 ) -> Optional[dict]:
-    """
+    """Get the concepts data from the ODEs defined in the code snippet
 
     Parameters
     ----------
@@ -76,7 +142,8 @@ def get_concepts_from_odes(
     Returns
     -------
     :
-        The concepts data in the form of a dictionary
+        The concepts data in the form of a dictionary, or None if no concepts data
+        could be generated.
     """
     # Cut out the part of the code that defines the `odes` variable
     # Regular expression to capture the `odes` variable assignment and definition
@@ -87,6 +154,7 @@ def get_concepts_from_odes(
     else:
         raise ValueError("No code snippet defining the variable `odes` found")
 
+    # Prompt the OpenAI chat completion to create the concepts data dictionary
     prompt = ODE_CONCEPTS_PROMPT_TEMPLATE.substitute(ode_insert=odes_code)
     response = client.run_chat_completion(prompt)
 
@@ -106,6 +174,24 @@ def execute_template_model_from_sympy_odes(
     attempt_grounding: bool,
     client: OpenAIClient,
 ) -> TemplateModel:
+    """Create a TemplateModel from the sympy ODEs defined in the code snippet string
+
+    Parameters
+    ----------
+    ode_str :
+        The code snippet defining the ODEs
+    attempt_grounding :
+        Whether to attempt grounding the concepts in the ODEs. This will prompt the
+        OpenAI chat completion to create concepts data to provide grounding for the
+        concepts in the ODEs. The concepts data is then used to create the TemplateModel.
+    client :
+        The OpenAI client
+
+    Returns
+    -------
+    :
+        The TemplateModel created from the sympy ODEs.
+    """
     # FixMe, for now use `exec` on the code, but need to find a safer way to execute
     #  the code
     # Import sympy just in case the code snippet does not import it
