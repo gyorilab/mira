74f9feb95c8503353aa05221a32a658a30042b5c
nanglo123 <tnanglo1998@gmail.com>
2025-02-12 16:31:22 -0500

    Adjust methods to write output to write to xml, process transitions

--- mira/modeling/sbml/export.py
+++ mira/modeling/sbml/export.py
@@ -7,7 +7,8 @@ from copy import deepcopy
 from typing import Dict, List, Optional
 
 from pydantic import BaseModel, Field, ConfigDict
-from libsbml import SBMLDocument
+from libsbml import SBMLDocument, parseL3Formula, writeSBMLToString
+import xml.etree.ElementTree as ET
 
 from mira.metamodel import expression_to_mathml, TemplateModel, SympyExprStr
 from mira.sources.amr import sanity_check_amr
@@ -15,10 +16,10 @@ from mira.sources.sbml import sbml_model_from_file
 from mira.modeling import Model
 from mira.sources.biomodels import get_template_model
 
+
 class SBMLModel:
 
     def __init__(self, model: Model):
-
 
         self.properties = {}
         self.initials = []
@@ -31,11 +32,14 @@ class SBMLModel:
         self.observables = []
         self.model_name = 'Model'
         self.model_description = 'Description'
+        self.sbml_xml = ""
 
-        sbml_document = SBMLDocument(model.template_model.sbml_level,
-                                model.template_model.sbml_version)
+        sbml_level = model.template_model.sbml_level
+        sbml_version = model.template_model.sbml_version
+        sbml_document = SBMLDocument(sbml_level, sbml_version)
         sbml_model = sbml_document.createModel()
 
+        # .parameters, .compartments, .species, .function_definitions, .rules, .reactions
         model_anns = model.template_model.annotations
         if model_anns:
             if model_anns.name:
@@ -44,7 +48,7 @@ class SBMLModel:
                 self.model_description = model_anns.description.examples[0]
 
         for model_key, model_param in model.parameters.items():
-            if not isinstance(model_key,str):
+            if not isinstance(model_key, str):
                 continue
             sbml_param = sbml_model.createParameter()
             sbml_param.setId(model_param.key)
@@ -61,8 +65,35 @@ class SBMLModel:
             if model_param.distribution:
                 pass
 
+        for key, transition in model.transitions.items():
+            sbml_reaction = sbml_model.createReaction()
 
+            if transition.template.name:
+                sbml_reaction.setId(transition.template.name)
+            elif transition.template.display_name:
+                sbml_reaction.setId(transition.template.display_name)
 
+            for reactant in transition.consumed:
+                sbml_reaction_reactant = sbml_reaction.createReactant()
+                sbml_reaction_reactant.setId(reactant.concept.name)
+                sbml_reaction_reactant.setSpecies(reactant.concept.name)
+
+            for product in transition.produced:
+                sbml_reaction_product = sbml_reaction.createProduct()
+                sbml_reaction_product.setId(product.concept.name)
+                sbml_reaction_product.setSpecies(product.concept.name)
+
+            for modifier in transition.control:
+                sbml_reaction_modifier = sbml_reaction.createModifier()
+                sbml_reaction_modifier.setId(modifier.concept.name)
+                sbml_reaction_modifier.setSpecies(modifier.concept.name)
+
+            rate_law = parseL3Formula(str(transition.template.rate_law))
+
+            kinetic_law = sbml_reaction.createKineticLaw()
+            kinetic_law.setMath(rate_law)
+
+        self.sbml_xml = writeSBMLToString(sbml_document)
 
     def to_xml(
         self,
@@ -90,11 +121,11 @@ class SBMLModel:
         : JSON
             A JSON dict representing the Petri net model.
         """
-        return name
+        return self.sbml_xml
 
     def to_xml_file(self, fname, name=None, description=None,
                     model_version=None, **kwargs):
-        """Write the Petri net model to a JSON file
+        """Write the SBML model to a xml file
 
         Parameters
         ----------
@@ -107,17 +138,17 @@ class SBMLModel:
         model_version : str, optional
             The version of the model.
         kwargs :
-            Additional keyword arguments to pass to :func:`json.dump`.
+            Additional keyword arguments to pass to :func:`tree.write`.
         """
-        indent = kwargs.pop('indent', 1)
-        js = self.to_xml(name=name, description=description,
+        root = self.to_xml(name=name, description=description,
                          model_version=model_version)
+        xml_tree = ET.ElementTree(root)
         with open(fname, 'w') as fh:
-            json.dump(js, fh, indent=indent, **kwargs)
+            xml_tree.write(fh, xml_declaration=True, **kwargs)
 
 
 def template_model_to_sbml(tm: TemplateModel):
-    """Convert a template model to a PetriNet JSON dict.
+    """Convert a template model to a SBML xml string.
 
     Parameters
     ----------
@@ -126,14 +157,13 @@ def template_model_to_sbml(tm: TemplateModel):
 
     Returns
     -------
-    A JSON dict representing the PetriNet model.
+    An xml string representing the SBML model.
     """
-    model = Model(tm)
     return SBMLModel(Model(tm)).to_xml()
 
 
 def template_model_to_petrinet_sbml_file(tm: TemplateModel, fname):
-    """Convert a template model to a PetriNet JSON file.
+    """Convert a template model to a SBML xml file.
 
     Parameters
     ----------
@@ -143,4 +173,3 @@ def template_model_to_petrinet_sbml_file(tm: TemplateModel, fname):
         The file name to write to.
     """
     SBMLModel(Model(tm)).to_xml_file(fname)
-
