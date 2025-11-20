3b31c64571512dbc66795c169804605f98d3c91b
nanglo123 <tnanglo1998@gmail.com>
2025-11-20 13:00:31 -0500

    Install VLM model also, only install pipeline models from mineru.utils.enum_class.ModelPath object class

--- docker/run_on_docker.sh
+++ docker/run_on_docker.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+# Build base image
 docker build -f Dockerfile -t mira_epi_dkg:base .
 
+# Build full image
 docker build -f Dockerfile.full -t mira_epi_dkg:full .
 
 # Either edit this line to add OPENAI_API_KEY env variable or export it in container
-docker run --detach -p 8771:8771 -p 7687:7687 -e MIRA_NEO4J_URL=bolt://0.0.0.0:7687 --name mira mira_epi_dkg:full
+docker run --detach -p 8771:8771 -p 7687:7687 -p 5000:5000 -e MIRA_NEO4J_URL=bolt://0.0.0.0:7687 --name mira mira_epi_dkg:full
